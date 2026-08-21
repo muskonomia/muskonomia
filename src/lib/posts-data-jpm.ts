64262ded85730693618a44aa7675e06a85035eff
muskonomiapl <muskonomia.pl@gmail.com>
2026-08-21 15:10:00 +0100

    Add JPMorgan Fremont post: Tesla holds back Model Y in Robotaxi

---
+++ src/lib/posts-data-jpm.ts
@@ -0,0 +1,34 @@
+import type { Post } from "./posts";
+
+export const postsJpm: Post[] = [
+{
+    slug: "jpmorgan-po-wizycie-w-fremont-tesla-wstrzymuje-model-y-w-robotaxi",
+    kicker: "Tesla",
+    topic: "tesla",
+    title: "JPMorgan po wizycie w Fremont: Tesla celowo wstrzymuje Model Y w Robotaxi",
+    excerpt: "Notatka po spotkaniu we Fremont: Model Y tylko most do Cybercaba, FSD V15 jako brama do unsupervised, Optimus SoP w najbli\u017cszych miesi\u0105cach. Cel: 475 USD.",
+    date: "21 sierpnia 2026",
+    isoDate: "2026-08-21",
+    img: "/img/jpm-fremont.jpg",
+    body: [
+      { type: "p", text: "Analitycy JPMorgan po niedawnym spotkaniu z Tesl\u0105 we Fremont i zwiedzaniu fabryki opublikowali notatk\u0119, kt\u00f3ra daje sporo konkret\u00f3w na temat plan\u00f3w firmy." },
+      { type: "img", src: "/img/jpm-fremont.jpg", alt: "Fabryka Tesli we Fremont, Kalifornia", caption: "Fremont: tu JPMorgan ogl\u0105da\u0142 lini\u0119 i s\u0142ucha\u0142 zarz\u0105du. St\u0105d wysz\u0142a notatka." },
+      { type: "h2", text: "Robotaxi i Cybercab" },
+      { type: "p", text: "Tesla \u015bwiadomie ogranicza liczb\u0119 Modeli Y w flocie robotaxi. Pow\u00f3d? Firma jest przekonana, \u017ce w kr\u00f3tkim terminie b\u0119dzie w stanie skalowa\u0107 Cybercaba. Model Y pe\u0142ni na razie rol\u0119 pomostow\u0105, a w\u0142a\u015bciwym pojazdem do masowego wdro\u017cenia unsupervised ma by\u0107 dedykowany Cybercab." },
+      { type: "img", src: "/img/jpm-modely-robotaxi.jpg", alt: "Tesla Model Y jako robotaxi na ulicy", caption: "Model Y w roli pomostu. Tesla nie chce pompowa\u0107 floty Y, skoro Cybercab ma wej\u015b\u0107 szybko." },
+      { type: "p", text: "Zarz\u0105d podkre\u015bli\u0142 te\u017c, \u017ce Cybercab to dopiero pierwszy form factor. W przysz\u0142o\u015bci pojawi\u0105 si\u0119 kolejne typy pojazd\u00f3w na tej samej platformie \u2014 jako przyk\u0142ad wskazano demonstracj\u0119 Robovana z wydarzenia 10/10." },
+      { type: "img", src: "/img/jpm-cybercab.jpg", alt: "Tesla Cybercab na ulicy, bez lusterek i klamki", caption: "Cybercab na drodze: bez kierownicy, bez peda\u0142\u00f3w. To ma by\u0107 masowe unsupervised, nie Y." },
+      { type: "h2", text: "FSD V15 \u2014 skok jako\u015bci" },
+      { type: "p", text: "Tesla traktuje FSD V15 jako wyra\u017any skok wydajno\u015bci, por\u00f3wnywalny do przej\u015bcia z V13 na V14. Aktualizacja obejmuje siedem kluczowych technologii. Oko\u0142o 40% z nich jest ju\u017c testowanych we flocie robotaxi, a wst\u0119pne feedbacki s\u0105 zach\u0119caj\u0105ce." },
+      { type: "p", text: "G\u0142\u00f3wny nacisk k\u0142adziony jest na minimalizowanie regresji w podstawowych funkcjach jazdy przy jednoczesnym dodawaniu nowych mo\u017cliwo\u015bci. V15 jest postrzegane jako podstawowa brama do skalowania unsupervised FSD." },
+      { type: "img", src: "/img/jpm-fsd.jpg", alt: "Wizualizacja FSD na ekranie Tesli", caption: "V15: siedem technologii, 40% ju\u017c w flocie robotaxi. Brama do unsupervised." },
+      { type: "p", text: "Obecny stack AI/HW4 radzi sobie z V15 i unsupervised, ale Tesla przygotowuje ju\u017c system AI4.5. Ma on zapewni\u0107 wi\u0119ksz\u0105 moc obliczeniow\u0105 (ok. 10% wy\u017csze FLOPS) i dwukrotnie wi\u0119cej pami\u0119ci \u2014 na wypadek rosn\u0105cych wymaga\u0144 modeli robotaxi i rozszerzaj\u0105cych si\u0119 context window." },
+      { type: "h2", text: "Optimus" },
+      { type: "p", text: "Robot humanoidalny jest na dobrej drodze do rozpocz\u0119cia produkcji seryjnej (SoP) w najbli\u017cszych miesi\u0105cach. Sprzeda\u017c komercyjna mo\u017ce ruszy\u0107 ju\u017c w drugiej po\u0142owie 2027 roku. Prezentacja Gen 3 zostanie zsynchronizowana bli\u017cej startu produkcji, \u017ceby chroni\u0107 przewag\u0119 konkurencyjn\u0105. Zakres Gen 4 (mo\u017cliwo\u015bci, koszt, skalowalno\u015b\u0107) b\u0119dzie wynika\u0142 z do\u015bwiadcze\u0144 zebranych podczas eksploatacji Gen 3." },
+      { type: "img", src: "/img/jpm-optimus.jpg", alt: "Pi\u0119\u0107 robot\u00f3w Tesla Optimus w hali", caption: "Optimus: SoP w najbli\u017cszych miesi\u0105cach, sprzeda\u017c komercyjna od H2 2027. Gen 3 bez wcze\u015bniejszej prezentacji." },
+      { type: "h2", text: "Rekomendacja" },
+      { type: "p", text: "JPMorgan po wizycie utrzyma\u0142 cen\u0119 docelow\u0105 Tesli na poziomie 475 dolar\u00f3w." },
+      { type: "p", text: "Notatka potwierdza kilka wa\u017cnych kierunk\u00f3w: priorytet dla Cybercaba w robotaxi, V15 jako kluczowy krok do unsupervised oraz realny, cho\u0107 wci\u0105\u017c wymagaj\u0105cy czasu, harmonogram Optimusa." },
+    ],
+  }
+];
