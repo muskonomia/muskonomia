4a329c00a6d9ca0b489d7bbf430637d36bb55efa
muskonomiapl <muskonomia.pl@gmail.com>
2026-08-21 15:30:10 +0100

    Prepend Starlink 11000 post to postsData

--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -1,7 +1,8 @@
 import type { Post } from "./posts";
+import { postsSl } from "./posts-data-sl";
 import { postsJpm } from "./posts-data-jpm";
 import { posts0 } from "./posts-data-0";
 import { postsA } from "./posts-data-a";
 import { postsB } from "./posts-data-b";
 
-export const postsData: Post[] = [...postsJpm, ...posts0, ...postsA, ...postsB];
+export const postsData: Post[] = [...postsSl, ...postsJpm, ...posts0, ...postsA, ...postsB];
