0200653532210e89b5d0795a7b9c201196b8e978
muskonomiapl <muskonomia.pl@gmail.com>
2026-08-21 15:10:07 +0100

    Prepend JPMorgan post to postsData

--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -1,6 +1,7 @@
 import type { Post } from "./posts";
+import { postsJpm } from "./posts-data-jpm";
 import { posts0 } from "./posts-data-0";
 import { postsA } from "./posts-data-a";
 import { postsB } from "./posts-data-b";
 
-export const postsData: Post[] = [...posts0, ...postsA, ...postsB];
+export const postsData: Post[] = [...postsJpm, ...posts0, ...postsA, ...postsB];
