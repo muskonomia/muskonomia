a821688aec1328946f1e249d00125ea574df3a14
muskonomia <sebastian.lipinski@gmail.com>
2026-08-29 09:47:06 -0700

    Wpis: Ashok Elluswamy podbija Cybercaba na 3rd Street

---
+++ src/lib/posts-data-ashok-cybercab.ts
@@ -0,0 +1,64 @@
+import type { Post } from "./posts";
+
+export const postsAshokCybercab: Post[] = [
+  {
+    slug: "ashok-cybercab-austin-bez-kierowcy",
+    kicker: "Tesla",
+    topic: "tesla",
+    title: "Ashok Elluswamy podbija Cybercaba na 3rd Street. Jedno zdanie, bez „first”",
+    excerpt:
+      "Szef AI Tesli cytuje 38-sekundowy film z Austin: „The streets won’t be the same anymore.” Niemal milion wyświetleń. Słów first i no monitor w jego poście nie ma.",
+    date: "29 sierpnia 2026",
+    isoDate: "2026-08-29",
+    img: "/img/ashok-cybercab-austin-poster.jpg",
+    body: [
+      {
+        type: "p",
+        text: "29 sierpnia 2026 o 05:45 UTC Ashok Elluswamy, w bio „Leading AI @Tesla”, cytuje film z Austin i pisze dokładnie: „The streets won’t be the same anymore.” Około 7,9 tys. polubień, niemal milion wyświetleń. W tym zdaniu nie ma first. Nie ma safety monitor. Nie ma startu usługi. https://x.com/aelluswamy/status/2093575830480077005",
+      },
+      {
+        type: "video",
+        src: "/video/ashok-cybercab-austin.mp4",
+        poster: "/img/ashok-cybercab-austin-poster.jpg",
+        caption: "38 s, Austin. Złoty Cybercab, napis na drzwiach, puste fotele. Film @artsimage, 29 sierpnia 00:20 UTC.",
+      },
+      {
+        type: "p",
+        text: "Film wrzucił pięć godzin wcześniej fotograf i inwestor Art Guajardo (@artsimage), z @AdanGuajardo. To nie jest konto Tesli. First, no monitor, no steering wheel, no pedals: to podpis fotografa, nie Tesli. https://x.com/artsimage/status/2093494079271166454",
+      },
+      {
+        type: "p",
+        text: "Ashok tego nie powtórzył. Podbił obraz i zostawił jedno zdanie o ulicach. Głos Tesli w tym wpisie kończy się na will not be the same. Reszta podpisu należy do człowieka z kamerą. Sam film @artsimage ma ok. 940 polubień. Cytat Ashoka ma prawie osiem tysięcy i milion odtworzeń. Liczba, która zmienia wagę kadru, nie jest w podpisie fotografa. Jest w tym, kto go podbił.",
+      },
+      {
+        type: "p",
+        text: "Co widać w 38 sekundach, nie w tweecie. Złoty dwudrzwiowy Cybercab, czarny napis na drzwiach, pełne kołpaki, cienki pasek świateł, brak lusterek. Przez szybę: dwa fotele, puste. Kierownicy z tej kamery nie widać. Pedałów z zewnątrz nie da się potwierdzić. Tablica teksańska. Znak ulicy: W 3rd St i San Antonio St, Austin. Zielone światło, pas rowerowy, hulajnoga, śmietnik, garaż z neonem PARK, witryna sugar wolf. W kadrze inne auta, w tym biały Model Y z naprzeciwka. To nie tor. To skrzyżowanie.",
+      },
+      {
+        type: "p",
+        text: "Kamera jedzie za autem, z poziomu ulicy. W prawym dolnym rogu znak @artsimage, dwa trójkąty. Nie ma ujęcia z wnętrza, nie ma rąk na kierownicy, nie ma kogoś na fotelu pasażera. Jest karoseria w ruchu i puste fotele za szybą. Tyle pikseli. Reszta to podpis.",
+      },
+      {
+        type: "ul",
+        items: [
+          "Ashok Elluswamy, 29.08 05:45 UTC: jedno zdanie, cytat filmu, ~7,9 tys. polubień, ~1 mln wyświetleń",
+          "Film: 38 s, 00:20 UTC, @artsimage z @AdanGuajardo, nie Tesla",
+          "W kadrze: złoty Cybercab, puste fotele, 3rd Street, ruch uliczny",
+          "„First / no monitor / no pedals” jest w podpisie fotografa, nie w poście Ashoka",
+        ],
+      },
+      {
+        type: "p",
+        text: "Na muskonomia.pl wisi już teaser Tesli na 3 września w Austin, Exclusive Access. Ten kadr nie jest tamtym eventem. Event może być tłem: za pięć dni firma woła ludzi na launch. 29 sierpnia szef AI podbija złote auto, które już jedzie przez 3rd Street, z pustymi fotelami w oknie.",
+      },
+      {
+        type: "p",
+        text: "Nie ma tu oficjalnego startu robotaxi. Nie ma oświadczenia Tesli, że w aucie nie siedzi monitor. Jest 38 sekund złotej karoserii w ruchu, podpis fotografa i jedno zdanie człowieka, który w Tesli prowadzi AI. Tyle widać. Tyle wolno napisać.",
+      },
+      {
+        type: "p",
+        text: "Ashok nie ogłosił ulicy. Podbił ją. Fotograf napisał historię. Szef AI napisał pogodę.",
+      },
+    ],
+  },
+];
