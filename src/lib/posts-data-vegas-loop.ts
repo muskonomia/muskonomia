ab0adfc67b54dc8fe6eb6c8364ab856943d2357c
muskonomia <sebastian.lipinski@gmail.com>
2026-08-29 10:00:21 -0700

    Hotfix: Vegas Loop — przepisany tekst po polsku

--- src/lib/posts-data-vegas-loop.ts
+++ src/lib/posts-data-vegas-loop.ts
@@ -7,7 +7,7 @@ export const postsVegasLoop: Post[] = [
     topic: "inne",
     title: "Vegas Loop dostał 19 nowych stacji. Na papierze jest ich już 123",
     excerpt:
-      "Clark County zatwierdził dodatkowe 19 stacji Vegas Loop. The Boring Company ma teraz zgody na 123, przy kilkunastu otwartych.",
+      "Komisja zoningowa Clark County zatwierdziła 19 sierpnia dodatkowe stacje. The Boring Company ogłosiła to we wtorek. Łącznie 123 zgody, przy kilkunastu otwartych.",
     date: "25 sierpnia 2026",
     isoDate: "2026-08-25",
     img: "/img/vegas-loop-123-stacji.jpg",
@@ -15,43 +15,39 @@ export const postsVegasLoop: Post[] = [
     body: [
       {
         type: "p",
-        text: "We wtorek 25 sierpnia 2026 r. The Boring Company ogłosiła, że Clark County zatwierdził dodatkowe 19 stacji Vegas Loop. Firma ma teraz zgody na 123 przystanki. Pod ziemią tych dziewiętnastu jeszcze nie ma. Są na mapie.",
+        text: "We wtorek 25 sierpnia 2026 r. The Boring Company ogłosiła dodatkowe 19 stacji Vegas Loop. Komisja zoningowa Clark County zatwierdziła je 19 sierpnia, pisze Mick Akers w Las Vegas Review-Journal. Firma ma teraz zgody na 123 przystanki. Wcześniejszy plan liczył 104. https://www.reviewjournal.com/local/traffic/boring-co-s-vegas-loop-announces-approval-of-19-additional-stations-bringing-total-to-123-3869704/",
       },
       {
         type: "p",
-        text: "W oficjalnym poście firma dziękuje Clark County i dokłada slogan: skoro Loop jedzie z punktu do punktu, bez przystanków po drodze, w skrajnym przypadku stacja mogłaby stanąć przy każdym podjeździe. https://x.com/boringcompany/status/2092396130361606374",
+        text: "W komunikacie firma dziękuje władzom hrabstwa. Skoro auto jedzie prosto do celu, bez stacji po drodze, przystanek mógłby teoretycznie stanąć przy każdym podjeździe. https://x.com/boringcompany/status/2092396130361606374",
       },
       {
         type: "img",
         src: "/img/vegas-loop-123-stacji.jpg",
         alt: "Mapa Vegas Loop: trasy tuneli i stacje od downtown po lotnisko Harry Reid",
-        caption: "Mapa z posta The Boring Company, 25 sierpnia 2026 r. Czarne kropki to stacje, linie to tunele.",
+        caption: "Mapa z komunikatu The Boring Company, 25 sierpnia 2026 r. Czarne kropki to stacje, linie to tunele.",
         contain: true,
       },
       {
         type: "p",
-        text: "To zgoda planistyczna, nie bilet na stację. Hrabstwo dało firmie prawo budować te przystanki. Nie dało gotowego tunelu. Las Vegas Review-Journal pisze, że 19 stacji przeszło komisję zoningową 19 sierpnia, a firma ogłosiła to we wtorek. Relacja Micka Akersa: https://www.reviewjournal.com/local/traffic/boring-co-s-vegas-loop-announces-approval-of-19-additional-stations-bringing-total-to-123-3869704/",
-      },
-      {
-        type: "p",
-        text: "Częściowo wiadomo, gdzie te stacje mają stanąć. Review-Journal, w przedruku CDC Gaming, wymienia m.in. Grand Prix Plaza przy torze Formuły 1 i przystanek w Chinatown. Kilka stacji ma trafić na Tropicana Avenue, między Las Vegas Boulevard a Paradise Road. Dwie przy Tuscany. Jeszcze działka po dawnym Gordon Biersch na Paradise, na północ od Flamingo: tam Boring planuje stację i biuro. https://cdcgaming.com/brief/las-vegas-19-more-boring-co-loop-stations-gain-approval/",
+        text: "Część z tych 19 ma już adres. Review-Journal, w przedruku CDC Gaming, wymienia Grand Prix Plaza przy torze Formuły 1 i przystanek w Chinatown. Kilka stacji ma stanąć przy Tropicana Avenue, między Las Vegas Boulevard a Paradise Road, dwie przy Tuscany. Na Paradise Road, na północ od Flamingo, na działce po dawnym Gordon Biersch, Boring planuje stację i biuro. https://cdcgaming.com/brief/las-vegas-19-more-boring-co-loop-stations-gain-approval/",
       },
       {
         type: "ul",
         items: [
-          "19 nowych stacji zatwierdzonych 19 sierpnia przez komisję zoningową Clark County",
-          "123 stacje łącznie na zgodach (wcześniejszy plan: 104)",
-          "ok. 109 km tuneli w docelowej sieci; Tesla z punktu do punktu, bez przystanków po drodze",
+          "19 stacji zatwierdzonych 19 sierpnia 2026 r. przez komisję zoningową Clark County",
+          "123 stacje łącznie na zgodach, wcześniej 104",
+          "ok. 109 km tuneli w docelowej sieci; pasażer jedzie do celu bez stacji po drodze",
           "na mapie: Strip, downtown, Chinatown, Allegiant Stadium, lotnisko Harry Reid",
         ],
       },
       {
         type: "p",
-        text: "Liczy się nie 123 na papierze, tylko to, ile stacji już wozi ludzi. Teslarati szacuje około 11 otwartych przystanków i ponad 4 mln pasażerów od 2021 r. Review-Journal wymienia działające stacje przy Las Vegas Convention Center oraz przy Resorts World, Encore, Fontainebleau, Westgate i Sahara. Lotnisko jest na mapie i w budowie, nie w rozkładzie. https://www.teslarati.com/county-vote-hands-elon-musks-vegas-tunnel-network-a-huge-new-target/",
+        text: "Ile z tego już wozi pasażerów, to inna liczba. Teslarati szacuje około 11 otwartych stacji i ponad 4 mln pasażerów od 2021 r. Review-Journal wymienia działające przystanki przy Las Vegas Convention Center oraz przy Resorts World, Encore, Fontainebleau, Westgate i Sahara. Lotnisko Harry Reid jest na mapie i w budowie, nie w rozkładzie. https://www.teslarati.com/county-vote-hands-elon-musks-vegas-tunnel-network-a-huge-new-target/",
       },
       {
         type: "p",
-        text: "Sahara otworzyła stację 6 sierpnia 2026 r. przy Paradise Road. Godziny 7–21, bilety z kodem QR albo przez internet. Relacja FOX5 Las Vegas: https://www.fox5vegas.com/2026/08/06/vegas-loop-station-now-open-at-sahara-las-vegas/ To tempo, w jakim Loop rośnie na asfalcie: jeden hotel. 19 stacji na jednej sesji komisji to tempo, w jakim rośnie na papierze.",
+        text: "Sahara otworzyła stację 6 sierpnia 2026 r. przy Paradise Road. Czynna od 7 do 21, bilety z kodem QR albo przez internet, relacjonuje FOX5 Las Vegas. https://www.fox5vegas.com/2026/08/06/vegas-loop-station-now-open-at-sahara-las-vegas/",
       },
       {
         type: "p",
