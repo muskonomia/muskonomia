ab9e51497266d5014d926f54aab78c6d1b90f2f9
muskonomia <sebastian.lipinski@gmail.com>
2026-09-01 14:55:00 -0700

    EMP: NFOŚiGW objął akcje za blisko 4,5 mld zł z KPO

---
+++ src/lib/posts-data-emp.ts
@@ -0,0 +1,81 @@
+import type { Post } from "./posts";
+
+export const postsEmp: Post[] = [
+  {
+    slug: "nfosigw-emp-45-mld-kpo-hub-jaworzno",
+    kicker: "Hub bez auta",
+    topic: "inne",
+    title: "NFOŚiGW został właścicielem EMP za blisko 4,5 mld zł",
+    excerpt:
+      "NFOŚiGW objął 26 sierpnia akcje EMP i przelał spółce blisko 4,5 mld zł z KPO. To największa inwestycja Funduszu. NaszEauto się skończyło. W lipcu BEV miały w Polsce 4 proc. rynku i spadek o 41 proc. rok do roku. Produkcja planowana jest na 2029. JV z Foxconnem wciąż jest planem.",
+    date: "1 września 2026",
+    isoDate: "2026-09-01",
+    img: "/img/emp-ceremonia.jpg",
+    body: [
+      {
+        type: "p",
+        text: "31 sierpnia Fundusz napisał, że 26 sierpnia objął akcje ElectroMobility Poland i przelał spółce blisko 4,5 mld zł z KPO. To największa inwestycja w historii NFOŚiGW. Dopłata 40 tys. zł do osobowego elektryka już się skończyła. W lipcu elektryki miały w Polsce 4 proc. rynku.",
+      },
+      {
+        type: "img",
+        src: "/img/emp-ceremonia.jpg",
+        alt: "Podpisanie umowy KPO na Polski Hub Elektromobilności, 13 maja 2026",
+        caption: "13 maja 2026. Na ekranie: 4,5 mld zł z KPO. Fot. Ministerstwo Aktywów Państwowych.",
+      },
+      {
+        type: "p",
+        text: "[Wnp](https://www.wnp.pl/przemysl/polski-samochod-elektryczny-coraz-blizej-nfosigw-zainwestowal-blisko-4-5-mld-zl,1094520.html) i [Interia](https://motoryzacja.interia.pl/wiadomosci/news-4-5-mld-zl-na-polskie-auto-elektryczne-emp-dostala-przelew-n,nId,23536650) oddają komunikat: Polski Hub Elektromobilności i „polskie auto elektryczne”. Nie marka Izera. EMP powstała w 2016 roku pod tę markę. Udziały miały PGE, Energa, Enea i Tauron. Fabryki nie zbudowano. Działkę 117 ha na Jaworznickim Obszarze Gospodarczym spółka ma od końca 2023 roku, [przypomina money.pl](https://www.money.pl/mobility/rzadowy-nfosigw-dosypal-miliardy-do-izery-2-0-jest-data-startu-produkcji-elektrykow-7324428700510272a.html).",
+      },
+      {
+        type: "p",
+        text: "Pieniądze poszły z programu Instrument wsparcia dla gospodarki niskoemisyjnej. 13 maja Fundusz i EMP podpisały umowę inwestycyjną i umowę akcjonariuszy, [pisała wtedy Gazeta](https://next.gazeta.pl/next/7,151003,32787423,rzad-inwestuje-jeszcze-4-5-mld-zl-w-polskie-elektryki-w-jaworznie.html). Walne podniosło kapitał, zmieniło statut i wpuściło do rady nadzorczej dwóch ludzi z NFOŚiGW.",
+      },
+      {
+        type: "img",
+        src: "/img/emp-podpis.jpg",
+        alt: "Podpisywanie umów NFOŚiGW i EMP",
+        caption: "Umowa inwestycyjna i umowa akcjonariuszy. Fot. MAP.",
+      },
+      {
+        type: "p",
+        text: "Hub w Jaworznie ma być zakładem, zapleczem B+R, oprogramowaniem i częściami do baterii. Fundusz planuje start produkcji na 2029 rok. Cały projekt: 2026–2030. Po starcie około 70 proc. wartości części ma pochodzić z UE, część z Polski. To plan, nie otwarcie hali.",
+      },
+      {
+        type: "p",
+        text: "EMP chce zrobić joint venture z Hon Hai (Foxconn) i Foxtron Vehicle Technologies. Umowę JV planuje na jesień 2026, budowę na wiosnę 2027. Podpisanej spółki jeszcze nie ma.",
+      },
+      {
+        type: "img",
+        src: "/img/emp-hennig.jpg",
+        alt: "Paulina Hennig-Kloska podczas ogłoszenia finansowania z KPO",
+        caption: "Paulina Hennig-Kloska o „podwójnych korzyściach EKO”. Fot. MAP.",
+      },
+      {
+        type: "p",
+        text: "Paulina Hennig-Kloska mówi o „podwójnych korzyściach EKO”. Dorota Zawadzka-Stępniak o łańcuchu wartości zeroemisyjnego transportu. Paweł Augustyn powtarza: największa inwestycja w historii NFOŚiGW.",
+      },
+      {
+        type: "img",
+        src: "/img/emp-gronkiewicz.jpg",
+        alt: "Cyprian Gronkiewicz, prezes EMP",
+        caption: "Cyprian Gronkiewicz, prezes EMP. Fot. MAP.",
+      },
+      {
+        type: "p",
+        text: "Ten sam Fundusz zamknął NaszEauto. Nabór skończył się 30 kwietnia 2026. 1,18 mld zł nie starczyło na wszystkie wnioski. Na osobówkę było do 40 tys. zł, z premią za złomowanie. Nowego programu dopłat nie ogłoszono.",
+      },
+      {
+        type: "p",
+        text: "[PZPM](https://www.pzpm.org.pl/pl/Elektromobilnosc/eRejestracje/LIPIEC-2026): w lipcu 2026 zarejestrowano 2258 osobowych BEV. O 41 proc. mniej niż rok wcześniej. Udział spadł z 7,6 proc. do 4 proc. Wysoka baza to między innymi NaszEauto. W Europie BEV wzięły w lipcu około 25,7 proc. rynku, [podaje Interia](https://motoryzacja.interia.pl/wiadomosci/news-europa-rzucila-sie-po-samochody-elektryczne-polska-czarna-pl,nId,23533300).",
+      },
+      {
+        type: "p",
+        text: "Na X najmocniej uderzyli w ten kontrast. Ryszard Wojtkowski (ok. 17 tys. wyświetleń) napisał, że publiczne pieniądze idą łatwo, a europejscy producenci elektryków ledwo zipią i bez sieci sprzedaży. Tomasz Wyłuda dodał, że nawet jeśli hala powstanie, produkcję i tak trzeba będzie dopłacać. Grzegorz Laskowski zestawił wcześniejsze zastrzyki z Funduszu Reprywatyzacji i obligacji z faktem, że w dniu przelewu nie było umowy z Foxconnem. Robert Gwiazdowski przypomniał, co o EMP mówiono, gdy spółkę zakładano.",
+      },
+      {
+        type: "p",
+        text: "Spółka bez seryjnego auta dostała największy czek w historii Funduszu. Kupujący osobówkę nie dostanie już 40 tys. zł.",
+      },
+    ],
+  },
+];
