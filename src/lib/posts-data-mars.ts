9cd6d259a8fe674e209d4f982b2221587232121c
muskonomia <sebastian.lipinski@gmail.com>
2026-09-01 20:48:04 +0000

    Mars article: lead only for Dima, body from February, drop iteration graf

--- src/lib/posts-data-mars.ts
+++ src/lib/posts-data-mars.ts
@@ -7,35 +7,31 @@ export const postsMars: Post[] = [
     topic: "spacex",
     title: "Baza na Marsie będzie jednym z największych wyzwań w historii",
     excerpt:
-      "Dima Zeniuk: baza na Marsie będzie jednym z największych wyzwań w historii ludzkości. Musk: Yes. Na Marsa da się lecieć co 26 miesięcy, lot trwa sześć.",
+      "Dima Zeniuk: budowa bazy na Marsie będzie jednym z największych wyzwań w historii ludzkości. Musk to potwierdził. Na Marsa da się lecieć co 26 miesięcy, lot trwa sześć.",
     date: "1 września 2026",
     isoDate: "2026-09-01",
     img: "/img/mars-alpha.jpg",
     body: [
       {
         type: "p",
-        text: "Dima Zeniuk napisał, że zbudowanie bazy na Marsie będzie jednym z największych wyzwań w historii ludzkości. Musk cytuje kadr i dopisuje: Yes. Liczbę, która to spina, sam podał w lutym: na Marsa da się lecieć tylko gdy planety zgrają się co 26 miesięcy, a lot trwa sześć.",
+        text: "[Dima Zeniuk](https://x.com/DimaZeniuk/status/2094647676927087024) napisał, że budowa bazy na Marsie będzie jednym z największych wyzwań w historii ludzkości. [Musk](https://x.com/elonmusk/status/2094849443506122939) to potwierdził. Na Marsa da się lecieć tylko gdy planety zgrają się co 26 miesięcy, a lot trwa sześć.",
       },
       {
         type: "img",
         src: "/img/mars-dima.jpg",
-        alt: "Grafika Dimy Zeniuka: baza na Marsie, którą Musk potwierdził",
+        alt: "Grafika Dimy Zeniuka: baza na Marsie",
         caption: "Kadr Dimy. Źródło: @DimaZeniuk.",
         contain: true,
       },
       {
         type: "p",
-        text: "[Dima Zeniuk](https://x.com/DimaZeniuk/status/2094647676927087024) napisał, że budowa bazy na Marsie będzie jednym z największych wyzwań w historii ludzkości. [Musk](https://x.com/elonmusk/status/2094849443506122939) to potwierdził.",
+        text: "W lutym, w wpisie o mieście na Księżycu, Musk rozrysował, dlaczego Mars jest wolniejszy. Na Księżyc można startować co 10 dni, lot trwa dwa. Na Marsa tylko przy zgraniu planet co 26 miesięcy, sześć miesięcy w drodze. Stąd jego własne liczby: miasto na Księżycu potencjalnie poniżej 10 lat, Mars 20 plus. Misja SpaceX zostaje ta sama. Miasto na Marsie ma ruszyć za 5–7 lat. Priorytet to zabezpieczenie cywilizacji, a Księżyc jest szybszy. Źródło: [wpis Muska z lutego](https://x.com/elonmusk/status/2020640004628742577).",
       },
       {
         type: "img",
         src: "/img/mars-alpha.jpg",
         alt: "Wizja SpaceX Mars Base Alpha: Starshipy na padzie, kopuła, łazik, droga",
         caption: "Base Alpha.",
-      },
-      {
-        type: "p",
-        text: "W lutym, w wpisie o mieście na Księżycu, rozrysował dlaczego Mars jest wolniejszy. Na Księżyc można startować co 10 dni, lot trwa dwa. Na Marsa tylko przy zgraniu planet co 26 miesięcy, sześć miesięcy w drodze. Stąd jego własne liczby: miasto na Księżycu potencjalnie poniżej 10 lat, Mars 20 plus. Misja SpaceX zostaje ta sama. Miasto na Marsie ma ruszyć za 5–7 lat. Priorytet to zabezpieczenie cywilizacji, a Księżyc jest szybszy. Źródło: [wpis Muska z lutego](https://x.com/elonmusk/status/2020640004628742577).",
       },
       {
         type: "img",
@@ -45,27 +41,23 @@ export const postsMars: Post[] = [
       },
       {
         type: "p",
-        text: "To nie jest rezygnacja. To jest tempo iteracji. Każde pudełko, które nie domknęło się na Marsie, czeka kolejne dwa lata na następne okno. Na Księżycu poprawkę da się polecieć w tym samym miesiącu.",
+        text: "Jest jeszcze twardsza rzecz. SpaceDaily pisał 27 sierpnia: po 13. locie testowym w lipcu SpaceX wciąż nie spróbował tankowania dwóch Starshipów na orbicie. Statek, który dochodzi na LEO, spala większość paliwa na wejście. Żeby iść dalej, tankowce muszą się zetknąć i przepchnąć superzimny metan i tlen. Progress tankował stacje od 1978, ale to była tona chemii, która stoi w temperaturze pokładowej. Tu chodzą setki ton dwóch cieczy, które ciągle chcą wrzeć. Źródło: [SpaceDaily](https://spacedaily.com/t-starship-large-scale-orbital-refuelling-mars/).",
       },
       {
         type: "img",
         src: "/img/mars-starbase.jpg",
         alt: "Starbase w Teksasie: stacked Starship przy wieży, ocean w tle",
-        caption: "Starbase. Stąd ma polecieć to, czego na Marsie jeszcze nie ma.",
+        caption: "Starbase.",
       },
       {
         type: "p",
-        text: "Jest jeszcze twardsza rzecz. SpaceDaily pisał 27 sierpnia: po 13. locie testowym w lipcu SpaceX wciąż nie spróbował tankowania dwóch Starshipów na orbicie. Statek, który dochodzi na LEO, spala większość paliwa na wejście. Żeby iść dalej, tankowce muszą się zetknąć i przepchnąć superzimny metan i tlen. Progress tankował stacje od 1978, ale to była tona chemii, która stoi w temperaturze pokładowej. Tu chodzą setki ton dwóch cieczy, które ciągle chcą wrzeć. Źródło: [SpaceDaily](https://spacedaily.com/t-starship-large-scale-orbital-refuelling-mars/).",
+        text: "Wewnętrzny transfer tlenu na locie 3, ponad 3 tony w jednym kadłubie, to nie jest demo tankowca. Dwa statki, dokowanie, zimne rury, powtórka. Tego jeszcze nie było.",
       },
       {
         type: "img",
         src: "/img/mars-kolonia.jpg",
         alt: "Widok z góry na kolonię marsjańską SpaceX: kopuła, tunele, trzy Starshipy",
         caption: "Kolonia z góry. To wciąż rysunek.",
-      },
-      {
-        type: "p",
-        text: "Wewnętrzny transfer tlenu na locie 3, ponad 3 tony w jednym kadłubie, to nie jest demo tankowca. Dwa statki, dokowanie, zimne rury, powtórka. Tego jeszcze nie było.",
       },
       {
         type: "p",
