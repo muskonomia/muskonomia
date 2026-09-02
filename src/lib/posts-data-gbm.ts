696863e56d49c883e62840952de428f8b55c7747
muskonomia <sebastian.lipinski@gmail.com>
2026-09-02 00:46:06 -0700

    Skróć wpis o sklepie z botami Groka

--- src/lib/posts-data-gbm.ts
+++ src/lib/posts-data-gbm.ts
@@ -7,7 +7,7 @@ export const postsGbm: Post[] = [
     topic: "xai",
     title: "Grok Bot dostanie sklep z gotowymi agentami",
     excerpt:
-      "SpaceXAI pokazała sklep z wybranymi botami Groka. Film zobaczyło 85 tys. osób, start zapowiadany jest na ten tydzień.",
+      "SpaceXAI pokazała sklep z wybranymi botami Groka. Start zapowiadany jest na ten tydzień.",
     date: "2 września 2026",
     isoDate: "2026-09-02",
     img: "/img/gbms-ui.jpg",
@@ -15,7 +15,7 @@ export const postsGbm: Post[] = [
     body: [
       {
         type: "p",
-        text: "We wtorek na X wylądował półminutowy film z nowym widokiem sklepu w Grok Bocie. [Pierwszy wpis](https://x.com/blankspeaker/status/2094912859428041049) zobaczyło 85 tys. osób. [X Freeze](https://x.com/XFreeze/status/2094915043783746027) podał go dalej i zebrał 1,6 tys. polubień. SpaceXAI ma puścić tę półkę jeszcze w tym tygodniu.",
+        text: "We wtorek na X wylądował półminutowy film [blankspeakera](https://x.com/blankspeaker/status/2094912859428041049) z nowym widokiem sklepu w Grok Bocie. SpaceXAI ma puścić ten sklep jeszcze w tym tygodniu.",
       },
       {
         type: "video",
@@ -25,27 +25,27 @@ export const postsGbm: Post[] = [
       },
       {
         type: "p",
-        text: "Marketplace siedzi w pasku po lewej, obok listy własnych botów. Wtyczki są tam od dawna. Nowa zakładka to Boty. Na górze leżą wyróżnione zestawy: Home robots Sawyera Merritta, Researchy Farzada, Forge Maria Nawfala i NYC Parent Dennisona Bertrama. Pod spodem kategorie: zespół Cursora, operacje i finanse, rekrutacja, sprzedaż, inżynieria, design, marketing, produkt.",
+        text: "I tak na przykład wejście w kartę pokazuje, jak bot ma pracować. Home robots Sawyera ma 8 pamięci i 7 umiejętności. Steruje z czatu kosiarką Segway Navimow, odkurzaczem Matic i robotami Matter. Rutyn i integracji na tym egzemplarzu jeszcze nie ma. Jeden przycisk dodaje go do zespołu. Potem bot sam się przedstawia i zaczyna składać własne umiejętności.",
+      },
+      {
+        type: "img",
+        src: "/img/gbms-ui.jpg",
+        alt: "Grok Bot z listą agentów i czatem",
+        caption: "Home robots Sawyera Merritta. Osiem pamięci, siedem umiejętności, jeden przycisk.",
       },
       {
         type: "p",
-        text: "Na liście Cursora widać dial bota i skippy’ego Matta Palmera, pisarza do maili i esejów, generator obrazów oraz franka Natashy Kuo, który ogarnia sale konferencyjne. Niżej są rekruter Tommy’ego Hansena, krytyk interfejsów, menedżer projektów Erica Zakariassona, bot do slajdów z rozmowy z klientem i partner do planowania imprez. W designie i marketingu leżą Imogen do briefów, Clip Bot do podpisanych wycinków z YouTube’a i copywriter, który ma pisać ludzkim głosem. W produkcie: ChatPRD Claire Vo i obserwator konkurencji. Jest też Best Video Editor od X Freeze.",
+        text: "[X Freeze](https://x.com/XFreeze/status/2094915043783746027) pokazał, że zamiast składać każdego agenta od zera, bierze się gotowy zestaw i dokłada rutyny oraz narzędzia.",
+      },
+      {
+        type: "img",
+        src: "/img/gbms-bot.jpg",
+        alt: "Grok Bot na komputerze i w telefonie",
+        caption: "Gotowy zestaw wchodzi do zespołu. Stanowisko, odprawa, rutyny.",
       },
       {
         type: "p",
-        text: "Wejście w kartę pokazuje, jak bot ma pracować. Home robots Sawyera ma 8 pamięci i 7 umiejętności. Steruje z czatu kosiarką Segway Navimow, odkurzaczem Matic i robotami Matter. Rutyn i integracji na tym egzemplarzu jeszcze nie ma. Jeden przycisk dodaje go do zespołu. Potem bot sam się przedstawia i zaczyna składać własne pamięci.",
-      },
-      {
-        type: "p",
-        text: "X Freeze napisał, o co tu chodzi: zamiast składać każdego agenta od zera, ludzie biorą gotowy zestaw, dokładają rutyny i narzędzia. Konto [Roundtable Space](https://x.com/RoundtableSpace/status/2095002526458470747) puściło to samo nagranie i doszło do 52 tys. wyświetleń. Cztery dni wcześniej to samo konto pisało o katalogu, który sam zbiera boty ze społeczności. Tamten wpis zobaczyło 50 tys. osób.",
-      },
-      {
-        type: "p",
-        text: "28 sierpnia blankspeaker pokazywał pierwszą wersję sklepu i pisał, że może wejść w następnym tygodniu. Tego dnia Grok Bot był na wersji 0.30.0, a marketplace siedział jeszcze na liście rzeczy niepuszczonych, obok rozmów głosowych i płatności Stripe. Dzielenie tożsamości bota działa od tamtego piątku. Sklep ma zebrać wybrane egzemplarze w jednym miejscu.",
-      },
-      {
-        type: "p",
-        text: "Wanda Wang po wtorkowym spotkaniu twórców botów wypisała, co już robią bez katalogu. Dają botom stanowiska. Każą im robić poranną odprawę. Stawiają rutyny. Jeden z trików: wgrać Grok Bota na maszynę, na której już siedzi Grok Bot, żeby agent sam odpalał kolejnych. Sklep ma skrócić drogę od takiego przepisu do jednego kliknięcia.",
+        text: "Na Marketplace boty dostaną stanowiska. Będą robić poranną odprawę. Dostaną rutyny. Jeden z trików: wgrać Grok Bota na maszynę, na której już siedzi Grok Bot, żeby agent sam odpalał kolejnych.",
       },
     ],
   },
