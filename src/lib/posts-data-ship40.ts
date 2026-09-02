7b112a9a6c0a4451dc4a40130dc981884597a8b2
muskonomia <sebastian.lipinski@gmail.com>
2026-09-02 00:31:39 -0700

    Filtry, RSS, sitemap, huby, stopka SEBIX, sync live posts.

---
+++ src/lib/posts-data-ship40.ts
@@ -0,0 +1,58 @@
+import type { Post } from "./posts";
+
+export const postsShip40: Post[] = [
+  {
+    slug: "spacex-ship-40-probki-oslony-polzanurzalny",
+    kicker: "SpaceX",
+    topic: "spacex",
+    title: "SpaceX ściągnął próbki osłony z Ship 40. Potem załadował go na półzanurzalny, do Starbase kilka miesięcy",
+    excerpt:
+      "27 sierpnia 2026 SpaceX zebrał próbki osłony termicznej Ship 40 u Christmas Island. Minutę później: kadłub na półzanurzalnym, kilka miesięcy drogi na Starbase.",
+    date: "27 sierpnia 2026",
+    isoDate: "2026-08-27",
+    img: "/img/spacex-ship40-plytki-inspekcja.jpg",
+    xPostId: "2092988521376059899",
+    body: [
+      {
+        type: "p",
+        text: "27 sierpnia 2026 SpaceX napisał, że inżynierowie spędzili kilka dni na inspekcji Ship 40 w wodach u Christmas Island: zebrali próbki osłony termicznej i „vital insight” z wejścia w atmosferę, a poprawki idą na kolejne pojazdy. Minutę później drugi oficjalny komunikat: Starship jest już na statku półzanurzalnym i zaczyna kilkumiesięczną drogę na Starbase.",
+      },
+      {
+        type: "p",
+        text: "W pierwszym poście (27.08, 14:52 UTC) są cztery nowe zdjęcia. Na kadłubie widać oznaczenie S40 i ekipę w pomarańczowych kapokach na czarnych, sześciokątnych płytkach. Na zbliżeniu inspektorzy leżą przy miejscu, gdzie osłona jest spieczona i odrapana, z jasną resztką. Firma nie mówi, czym ta resztka jest. Z rufy sterczą dysze Raptorów. Podpis jest suchy: kilka dni inspekcji, próbki osłony, wnioski z wejścia w atmosferę, poprawki na przyszłych pojazdach. [Post SpaceX na X](https://x.com/SpaceX/status/2092988521376059899)",
+      },
+      {
+        type: "img",
+        src: "/img/spacex-ship40-plytki-inspekcja.jpg",
+        alt: "Inspektorzy na sześciokątnych płytkach osłony Ship 40, kadłub w wodzie u Christmas Island",
+        caption: "Ship 40: ekipa na czarnych płytkach, spieczona osłona, jasna resztka. Zdjęcie z posta SpaceX, 27 sierpnia 2026.",
+      },
+      {
+        type: "p",
+        text: "Słowo, które tu pracuje, to samples. SpaceX nie mówi, że Ship 40 poleci drugi raz. Mówi, że z tej osłony zeszli z materiałem, który ma zmienić następne sztuki. Kadłub zostaje laboratorium, nie rakietą.",
+      },
+      {
+        type: "img",
+        src: "/img/spacex-ship40-raptory.jpg",
+        alt: "Rufa Ship 40: dysze Raptorów, kadłub w wodzie, wyspa w tle",
+        caption: "Z rufy sterczą Raptory. SpaceX nie podaje listy uszkodzeń płytka po płytce. Orig z tego samego zestawu.",
+      },
+      {
+        type: "p",
+        text: "Drugi post, 63 sekundy później, domyka logistykę: “Starship has now been loaded onto a semi-submersible ship to begin the several month journey back to Starbase.” SpaceX dziękuje też recovery team za “challenging conditions”. Nazwy jednostki nie podaje. [Drugi post SpaceX na X](https://x.com/SpaceX/status/2092988782622519728)",
+      },
+      {
+        type: "p",
+        text: "Relacje z 27 sierpnia, m.in. TeslaNorth, identyfikują półzanurzalny jako Forte: najpierw zalał balast, wsunął się pod 52-metrowy stopień, potem się wynurzył. [Relacja TeslaNorth](https://teslanorth.com/2026/08/27/spacex-ship-40-christmas/) ABC News z 28 sierpnia pisze, że Forte wyszedł z Christmas Island w piątek po południu, kurs na Teksas. Długość rejsu bierzemy z SpaceX (kilka miesięcy), nie z szacunków “kilku tygodni”. [Relacja ABC News](https://www.abc.net.au/news/2026-08-28/spacex-rocket-begins-journey-back-to-texas-from-christmas-island/107079554)",
+      },
+      {
+        type: "p",
+        text: "27 sierpnia SpaceX nie podaje daty cumowania, nie obiecuje catcha na wieży i nie ogłasza reuse tego egzemplarza. Nie ma listy uszkodzeń płytka po płytce ani werdyktu, czy osłona “zdała”. Jest zdanie o improvements now planned on future vehicles i zdjęcia ludzi, którzy po tych płytkach chodzili.",
+      },
+      {
+        type: "p",
+        text: "Jeśli Starship ma kiedyś wracać na wieżę, ta droga jest mniej widowiskowa niż catch: próbki w worku, kadłub na półzanurzalnym, kilka miesięcy oceanu. 27 sierpnia SpaceX nie powiedział, że wyłowił legendę. Powiedział, że ściągnął z niej to, z czego zbuduje następną.",
+      },
+    ],
+  },
+];
---
+++ src/lib/posts-data-vegas-loop.ts
@@ -0,0 +1,62 @@
+import type { Post } from "./posts";
+
+export const postsVegasLoop: Post[] = [
+  {
+    slug: "vegas-loop-19-nowych-stacji",
+    kicker: "Boring",
+    topic: "boring",
+    title: "Vegas Loop dostał 19 nowych stacji. Na papierze jest ich już 123",
+    excerpt:
+      "Clark County zatwierdził dodatkowe 19 stacji Vegas Loop. The Boring Company ma teraz zgody na 123, przy kilkunastu otwartych.",
+    date: "25 sierpnia 2026",
+    isoDate: "2026-08-25",
+    img: "/img/vegas-loop-123-stacji.jpg",
+    contain: true,
+    body: [
+      {
+        type: "p",
+        text: "We wtorek 25 sierpnia 2026 r. The Boring Company ogłosiła, że Clark County zatwierdził dodatkowe 19 stacji Vegas Loop. Firma ma teraz zgody na 123 przystanki. Pod ziemią tych dziewiętnastu jeszcze nie ma. Są na mapie.",
+      },
+      {
+        type: "p",
+        text: "W oficjalnym poście firma dziękuje Clark County i dokłada slogan: skoro Loop jedzie z punktu do punktu, bez przystanków po drodze, w skrajnym przypadku stacja mogłaby stanąć przy każdym podjeździe. [Post The Boring Company na X](https://x.com/boringcompany/status/2092396130361606374)",
+      },
+      {
+        type: "img",
+        src: "/img/vegas-loop-123-stacji.jpg",
+        alt: "Mapa Vegas Loop: trasy tuneli i stacje od downtown po lotnisko Harry Reid",
+        caption: "Mapa z posta The Boring Company, 25 sierpnia 2026 r. Czarne kropki to stacje, linie to tunele.",
+        contain: true,
+      },
+      {
+        type: "p",
+        text: "To zgoda planistyczna, nie bilet na stację. Hrabstwo dało firmie prawo budować te przystanki. Nie dało gotowego tunelu. Las Vegas Review-Journal pisze, że 19 stacji przeszło komisję zoningową 19 sierpnia, a firma ogłosiła to we wtorek. Relacja Micka Akersa: [Relacja Las Vegas Review-Journal](https://www.reviewjournal.com/local/traffic/boring-co-s-vegas-loop-announces-approval-of-19-additional-stations-bringing-total-to-123-3869704/)",
+      },
+      {
+        type: "p",
+        text: "Częściowo wiadomo, gdzie te stacje mają stanąć. Review-Journal, w przedruku CDC Gaming, wymienia m.in. Grand Prix Plaza przy torze Formuły 1 i przystanek w Chinatown. Kilka stacji ma trafić na Tropicana Avenue, między Las Vegas Boulevard a Paradise Road. Dwie przy Tuscany. Jeszcze działka po dawnym Gordon Biersch na Paradise, na północ od Flamingo: tam Boring planuje stację i biuro. [Relacja CDC Gaming](https://cdcgaming.com/brief/las-vegas-19-more-boring-co-loop-stations-gain-approval/)",
+      },
+      {
+        type: "ul",
+        items: [
+          "19 nowych stacji zatwierdzonych 19 sierpnia przez komisję zoningową Clark County",
+          "123 stacje łącznie na zgodach (wcześniejszy plan: 104)",
+          "ok. 109 km tuneli w docelowej sieci; Tesla z punktu do punktu, bez przystanków po drodze",
+          "na mapie: Strip, downtown, Chinatown, Allegiant Stadium, lotnisko Harry Reid",
+        ],
+      },
+      {
+        type: "p",
+        text: "Liczy się nie 123 na papierze, tylko to, ile stacji już wozi ludzi. Teslarati szacuje około 11 otwartych przystanków i ponad 4 mln pasażerów od 2021 r. Review-Journal wymienia działające stacje przy Las Vegas Convention Center oraz przy Resorts World, Encore, Fontainebleau, Westgate i Sahara. Lotnisko jest na mapie i w budowie, nie w rozkładzie. [Relacja Teslarati](https://www.teslarati.com/county-vote-hands-elon-musks-vegas-tunnel-network-a-huge-new-target/)",
+      },
+      {
+        type: "p",
+        text: "Sahara otworzyła stację 6 sierpnia 2026 r. przy Paradise Road. Godziny 7–21, bilety z kodem QR albo przez internet. Relacja FOX5 Las Vegas: [Relacja FOX5 Las Vegas](https://www.fox5vegas.com/2026/08/06/vegas-loop-station-now-open-at-sahara-las-vegas/) To tempo, w jakim Loop rośnie na asfalcie: jeden hotel. 19 stacji na jednej sesji komisji to tempo, w jakim rośnie na papierze.",
+      },
+      {
+        type: "p",
+        text: "Rzeczniczka Clark County Jennifer Cooper powiedziała Review-Journal, że The Boring Company nie ma przyspieszonej ścieżki pozwoleń. Hrabstwo rozpatruje wnioski tak szybko, jak potrafi. 19 stacji na liście nie skraca kolejki do wiertła.",
+      },
+    ],
+  },
+];
