8e23a8104cbe176b7fcedb72885241bf4995ccdf
muskonomia <sebastian.lipinski@gmail.com>
2026-09-02 00:43:00 -0700

    Filtry, RSS, sitemap, huby, stopka SEBIX, sync live posts.

--- src/lib/posts-data-c.ts
+++ src/lib/posts-data-c.ts
@@ -1,1 +1,6 @@
-PLACEHOLDER_LOAD_FROM_L02
+import type { Post } from "./posts";
+import { postsC1 } from "./posts-data-c1";
+import { postsC2 } from "./posts-data-c2";
+import { postsC3 } from "./posts-data-c3";
+
+export const postsC: Post[] = [...postsC1, ...postsC2, ...postsC3];
