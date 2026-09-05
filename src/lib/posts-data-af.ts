740e633dcf0515ac7b9ebf512b08a6cd7aa01c35
muskonomia <sebastian.lipinski@gmail.com>
2026-09-05 01:25:46 -0700

    Wpis: Grok Bot po cichu zostaje najlepszym sposobem na kodowanie z AI

---
+++ src/lib/posts-data-af.ts
@@ -0,0 +1,107 @@
+import type { Post } from "./posts";
+
+export const postsAf: Post[] = [
+  {
+    slug: "grok-bot-developer-i-pm",
+    kicker: "Grok Bot",
+    topic: "xai",
+    title: "Grok Bot po cichu zostaje najlepszym sposobem na kodowanie z AI",
+    excerpt:
+      "Finn składa w Grok Bocie dwa boty. Jeden koduje w chmurowych agentach Cursora. Drugi trzyma tablicę w Notion albo Linearze.",
+    date: "5 września 2026",
+    isoDate: "2026-09-05",
+    img: "/img/gbms-ui.jpg",
+    body: [
+      {
+        type: "p",
+        text: "[Alex Finn](https://x.com/AlexFinn/status/2095944004932432216) napisał:",
+      },
+      {
+        type: "p",
+        text: "Grok Bot po cichu zostaje najlepszym sposobem na kodowanie z AI.",
+      },
+      {
+        type: "img",
+        src: "https://pbs.twimg.com/media/HRZJ8TBbkAAq4R1.png",
+        alt: "Lista botów Finna: Hank deweloper, Kelly kierownik projektu, Reed badacz",
+        caption: "Hank koduje. Kelly prowadzi tablicę. Źródło: @AlexFinn.",
+        contain: true,
+      },
+      {
+        type: "p",
+        text: "Chmurowe agenty Cursora w Grok Bocie są rewelacyjne, ale trzeba ich używać we właściwy sposób.",
+      },
+      {
+        type: "p",
+        text: "Sekret jest taki.",
+      },
+      {
+        type: "p",
+        text: "Potrzebujesz bota dewelopera i bota do zarządzania projektem.",
+      },
+      {
+        type: "img",
+        src: "/img/gbms-ui.jpg",
+        alt: "Interfejs Grok Bota z listą agentów i czatem",
+        caption: "Grok Bot. Własny zespół agentów, nie jeden czat.",
+      },
+      {
+        type: "p",
+        text: "Bot deweloper zajmuje się wyłącznie kodem. Odpala chmurowe agenty Cursora.",
+      },
+      {
+        type: "img",
+        src: "/img/cb-cursorbench.jpg",
+        alt: "Cursor i testy agentów kodujących",
+        caption: "Chmurowe agenty Cursora. Deweloper w Grok Bocie odpala je do każdego zadania.",
+        contain: true,
+      },
+      {
+        type: "p",
+        text: "Kierownik projektu cały czas patrzy na dewelopera. Pilnuje, czy idzie po śladzie, i po drodze daje mu zgodę.",
+      },
+      {
+        type: "p",
+        text: "Bot kierownika projektu trzyma dewelopera przy tablicy. Tablica może siedzieć w Notion albo w Linearze. Oba mają w Grok Bocie bardzo dobre, darmowe wtyczki.",
+      },
+      {
+        type: "img",
+        src: "/img/gbms-bot.jpg",
+        alt: "Grok Bot na komputerze i w telefonie",
+        caption: "Tablica w Notion albo Linearze. Wtyczki w Grok Bocie są darmowe.",
+      },
+      {
+        type: "p",
+        text: "Na start kierownik bierze projekt, rozpisuje zadania w Notion albo Linearze i podaje je deweloperowi po kolei.",
+      },
+      {
+        type: "p",
+        text: "Deweloper bierze zadania i do każdego otwiera pull request. Ty testujesz i scalasz, kiedy boty pracują.",
+      },
+      {
+        type: "p",
+        text: "Ten obieg jest mocny z kilku powodów.",
+      },
+      {
+        type: "p",
+        text: "To pętla fabryki oprogramowania. Kod powstaje sam, a agenty pilnują, żeby nie zjechał z toru.",
+      },
+      {
+        type: "p",
+        text: "Chmurowe agenty Cursora są na tyle dobre, że kod wychodzi naprawdę wysokiej jakości.",
+      },
+      {
+        type: "p",
+        text: "Do tego wszystko jest poukładane, bo każde zadanie dostaje pull request śledzony w Notion albo Linearze.",
+      },
+      {
+        type: "p",
+        text: "W stu procentach polecam to złożyć przy następnym projekcie. Przeniósłem w ten obieg 90 proc. swojego kodowania z AI do Grok Bota.",
+      },
+      {
+        type: "p",
+        text: "Kod na skróty: wrzuć ten wpis swojemu botowi szefa sztabu i każ mu to zestawić.",
+      },
+    ],
+  },
+];
--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -1,1 +1,96 @@
-placeholder
+import type { Post } from "./posts";
+import { postsAf } from "./posts-data-af";
+import { postsSl5 } from "./posts-data-sl5";
+import { postsLrt } from "./posts-data-lrt";
+import { postsS5 } from "./posts-data-s5";
+import { postsAce } from "./posts-data-ace";
+import { postsCpub } from "./posts-data-cpub";
+import { postsRv } from "./posts-data-rv";
+import { postsCcol } from "./posts-data-ccol";
+import { postsS14 } from "./posts-data-s14";
+import { postsWsk } from "./posts-data-wsk";
+import { postsGbm } from "./posts-data-gbm";
+import { postsGbl } from "./posts-data-gbl";
+import { postsCca } from "./posts-data-cca";
+import { postsVw } from "./posts-data-vw";
+import { postsEmp } from "./posts-data-emp";
+import { postsMars } from "./posts-data-mars";
+import { postsGbms } from "./posts-data-gbms";
+import { postsG20 } from "./posts-data-g20";
+import { postsV3 } from "./posts-data-v3";
+import { postsFsd } from "./posts-data-fsd";
+import { postsUae } from "./posts-data-uae";
+import { postsOr } from "./posts-data-or";
+import { postsGx } from "./posts-data-gx";
+import { postsInv } from "./posts-data-inv";
+import { postsRo } from "./posts-data-ro";
+import { postsLa } from "./posts-data-la";
+import { postsBv } from "./posts-data-bv";
+import { postsPw } from "./posts-data-pw";
+import { postsTb } from "./posts-data-tb";
+import { postsVegasLoop } from "./posts-data-vegas-loop";
+import { postsShip40 } from "./posts-data-ship40";
+import { postsAshokCybercab } from "./posts-data-ashok-cybercab";
+import { postsScout } from "./posts-data-scout";
+import { postsGb } from "./posts-data-gb";
+import { postsSc } from "./posts-data-sc";
+import { postsCc } from "./posts-data-cc";
+import { postsKat } from "./posts-data-kat";
+import { postsNl } from "./posts-data-nl";
+import { postsCb } from "./posts-data-cb";
+import { postsZl } from "./posts-data-zl";
+import { postsG5 } from "./posts-data-g5";
+import { postsSl } from "./posts-data-sl";
+import { postsJpm } from "./posts-data-jpm";
+import { posts0 } from "./posts-data-0";
+import { postsA } from "./posts-data-a";
+import { postsB } from "./posts-data-b";
+
+export const postsData: Post[] = [
+  ...postsAf,
+  ...postsSl5,
+  ...postsLrt,
+  ...postsS5,
+  ...postsAce,
+  ...postsCpub,
+  ...postsRv,
+  ...postsCcol,
+  ...postsS14,
+  ...postsWsk,
+  ...postsGbm,
+  ...postsGbl,
+  ...postsCca,
+  ...postsVw,
+  ...postsEmp,
+  ...postsMars,
+  ...postsGbms,
+  ...postsG20,
+  ...postsV3,
+  ...postsFsd,
+  ...postsUae,
+  ...postsOr,
+  ...postsGx,
+  ...postsInv,
+  ...postsRo,
+  ...postsLa,
+  ...postsBv,
+  ...postsPw,
+  ...postsTb,
+  ...postsVegasLoop,
+  ...postsShip40,
+  ...postsAshokCybercab,
+  ...postsScout,
+  ...postsGb,
+  ...postsSc,
+  ...postsCc,
+  ...postsKat,
+  ...postsNl,
+  ...postsCb,
+  ...postsZl,
+  ...postsG5,
+  ...postsSl,
+  ...postsJpm,
+  ...posts0,
+  ...postsA,
+  ...postsB,
+];
