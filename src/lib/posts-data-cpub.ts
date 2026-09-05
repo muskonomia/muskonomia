8d5f67f60efc2f334b82f302669f225d005b68fe
muskonomia <sebastian.lipinski@gmail.com>
2026-09-04 18:35:28 -0700

    Aktualizacja wpisu cybercab-publiczne-przejazdy-austin

--- src/lib/posts-data-cpub.ts
+++ src/lib/posts-data-cpub.ts
@@ -5,16 +5,26 @@ export const postsCpub: Post[] = [
     slug: "cybercab-publiczne-przejazdy-austin",
     kicker: "Robotaxi",
     topic: "tesla",
-    title: "W Austin Cybercaba zamawia się już z aplikacji",
+    title: "W Austin Cybercab już działa",
     excerpt:
-      "Od 4 września Cybercaba w Austin zamawia się z aplikacji. Około 45 sztuk w rejestrze, trzy godziny jazdy za 92,51 dolara.",
+      "Od 4 września Cybercab jeździ w Austin z aplikacji. W rejestrze około 45 sztuk. Czas oczekiwania skoczył z kilku minut do ponad godziny. Sawyer: 92,51 dolara za trzy godziny vs ok. 200 Uberem.",
     date: "4 września 2026",
     isoDate: "2026-09-04",
-    img: "/img/cpub-sawyer.jpg",
+    img: "/img/cpub-drzwi.jpg",
     body: [
       {
         type: "p",
-        text: "Od 4 września złota dwójka bez kierownicy jeździ nie tylko dla gości eventu. Konto Tesla Robotaxi najpierw dało godzinę 17:00 czasu teksańskiego, potem przysunęło start do 14:00. Wezwanie idzie z tej samej aplikacji co Model Y. Źródło: [Tesla Robotaxi](https://x.com/robotaxi/status/2095707198873571734).",
+        text: "Od 4 września Cybercab już jeździ. Na ten moment w rejestrze jest około 45 Cybercabów. Ile z nich siedzi w puli na wezwanie, Tesla nie podała. Reszta Robotaxi w mieście to nadal Y-ki. Czas oczekiwania na Cybercaba skoczył z kilku minut do ponad godziny.",
+      },
+      {
+        type: "img",
+        src: "/img/cpub-drzwi.jpg",
+        alt: "Złoty Cybercab z otwartymi drzwiami i napisem Cybercab",
+        caption: "Dwa miejsca. Wejście tam, gdzie zwykle siedział kierowca.",
+      },
+      {
+        type: "p",
+        text: "[Sawyer Merritt](https://x.com/SawyerMerritt/status/2095993244945457239) po trzech godzinach w kabinie złożył rachunek: 92,51 dolara (ok. 343 zł). Te same trasy Uberem policzył na 200 dolarów (ok. 742 zł). Najdłuższy kurs: około 40 minut. Wsparcia nie dzwonił. Zarzut jeden, i to ten sam co przy Y-ku: punkt odbioru i zjazdu czasem wymaga dojścia piechotą.",
       },
       {
         type: "video",
@@ -24,28 +34,7 @@ export const postsCpub: Post[] = [
       },
       {
         type: "p",
-        text: "Teksas ma w rejestrze około **45** Cybercabów. Ile z nich siedzi w puli na wezwanie, Tesla nie podała. Reszta Robotaxi w mieście to nadal Y-ki. Gdy publiczność weszła do kolejki, czas oczekiwania na Cybercaba skoczył z kilku minut do ponad godziny, napisał Sawyer Merritt. Źródło: [Sawyer Merritt](https://x.com/SawyerMerritt/status/2095956167734202378).",
-      },
-      {
-        type: "img",
-        src: "/img/cpub-wait.jpg",
-        alt: "Aplikacja Tesla Robotaxi: czas oczekiwania na Cybercaba ponad godzinę",
-        caption: "Z 5–10 minut na ponad godzinę, od razu po otwarciu. Źródło: @SawyerMerritt.",
-        contain: true,
-      },
-      {
-        type: "p",
-        text: "Ten sam Sawyer złożył rachunek po trzech godzinach w kabinie: **92,51 dolara** (ok. **343 zł**). Te same trasy Uberem policzył na **200 dolarów** (ok. **742 zł**). Najdłuższy kurs: około 40 minut. Wsparcia nie dzwonił. Zarzut jeden, i to ten sam co przy Y-ku: punkt odbioru i zdania czasem wymaga dojścia piechotą. Źródło: [Sawyer Merritt](https://x.com/SawyerMerritt/status/2095993244945457239).",
-      },
-      {
-        type: "img",
-        src: "/img/cpub-drzwi.jpg",
-        alt: "Otwarte drzwi złotego Cybercaba z napisem Cybercab",
-        caption: "Dwa miejsca. Wejście tam, gdzie zwykle siedział kierowca.",
-      },
-      {
-        type: "p",
-        text: "Ryan McCaffrey z IGN dołożył siedem kursów i roboty drogowe. Auto, pisze, nie zgubiło się w prowizorycznych pasach. Źródło: [Ryan McCaffrey](https://x.com/DMC_Ryan/status/2096001088092790869).",
+        text: "[Ryan McCaffrey](https://x.com/DMC_Ryan/status/2096001088092790869) z IGN dołożył siedem kursów i roboty drogowe. Auto, pisze, nie zgubiło się w prowizorycznych pasach.",
       },
       {
         type: "img",
@@ -55,24 +44,17 @@ export const postsCpub: Post[] = [
       },
       {
         type: "p",
-        text: "Whole Mars wsiadł z Sawyera na dłuższą relację. Zack pokazał, że do centralnego ekranu da się podpiąć pady i pograć w trakcie jazdy. Oficjalny klip Tesli z eventu zostaje przy haśle o bezpieczeństwie. NHTSA, podaje Cybernews, jest z firmą w kontakcie i ocenia wdrożenie. Źródła: [Whole Mars](https://x.com/wholemars/status/2096033131308437777), [Zack](https://x.com/BLKMDL3/status/2095937091163042299), [Tesla](https://x.com/Tesla/status/2095647989637263684), [Cybernews](https://cybernews.com/tech/tesla-cybercab-limited-rides-texas-rollout/).",
+        text: "[Whole Mars](https://x.com/wholemars/status/2096033131308437777) wsiadł z Sawyerem na dłuższą relację. [Zack](https://x.com/BLKMDL3/status/2095937091163042299) pokazał, że do centralnego ekranu da się podpiąć pady i pograć w trakcie jazdy.",
       },
       {
         type: "img",
         src: "/img/cpub-recline.jpg",
-        alt: "Sawyer Merritt pokazuje rozkładane fotele Cybercaba",
+        alt: "Rozkładane fotele Cybercaba, miejsce kierowcy bez kierownicy",
         caption: "Odchyl fotel. To miejsce kierowcy. Źródło: @SawyerMerritt.",
       },
       {
-        type: "img",
-        src: "/img/cpub-lifetimes.jpg",
-        alt: "Grafika Tesli: Cybercab trenowany na ponad 16 000 żyć jazdy",
-        caption: "Ponad 16 000 żyć jazdy. Źródło: @cb_doge.",
-        contain: true,
-      },
-      {
         type: "p",
-        text: "To wciąż Austin i wciąż mała flota. Y wozi cztery osoby w sześciu miastach. Cybercab wozi dwie, na razie w jednym. Różnica nie siedzi w aplikacji. Siedzi w tym, że pasażer odchyla fotel w miejscu, gdzie zwykle siedział kierowca, i auto jedzie dalej.",
+        text: "Teraz czekamy na kolejne miasta.",
       },
     ],
   },
