aaa3bc15f39f0f51ca83a251dc07f337e7c27798
muskonomia <sebastian.lipinski@gmail.com>
2026-09-04 18:36:02 -0700

    Wpis: Starlink V5 wejdzie bezpośrednio do Cybercaba

---
+++ src/lib/posts-data-s5.ts
@@ -0,0 +1,72 @@
+import type { Post } from "./posts";
+
+export const postsS5: Post[] = [
+  {
+    slug: "starlink-v5-w-cybercabie",
+    kicker: "Robotaxi",
+    topic: "tesla",
+    title: "Starlink V5 wejdzie bezpośrednio do Cybercaba",
+    excerpt:
+      "Tesla pokazała kabinę Cybercaba z 22-calowym ekranem. Starlink potwierdza, że V5 wejdzie w auto wprost i utrzyma łączność tam, gdzie komórka słabnie.",
+    date: "4 września 2026",
+    isoDate: "2026-09-04",
+    img: "/img/cpub-fotel.jpg",
+    body: [
+      {
+        type: "p",
+        text: "Tesla wrzuciła 27-sekundowy [film z kabiny Cybercaba](https://x.com/Tesla/status/2095648795304263759). Wpis zebrał 7,3 mln wyświetleń. Na 22-calowym ekranie jest kino, muzyka i gry. [Starlink](https://x.com/Starlink/status/2095667919493779512) dopisał, że sprzęt V5 wejdzie w to auto wprost i utrzyma łączność także tam, gdzie sieć komórkowa słabnie albo się zapycha.",
+      },
+      {
+        type: "video",
+        src: "https://video.twimg.com/amplify_video/2095648643659317251/vid/avc1/3840x2160/5xYvmK5ICiE4O_T4.mp4",
+        poster: "/img/cpub-fotel.jpg",
+        caption: "Film Tesli z kabiny Cybercaba. 22 cale, bez kierownicy. Źródło: @Tesla.",
+      },
+      {
+        type: "p",
+        text: "Film jedzie nocą przez miasto. Kobieta zasypia na fotelu przy włączonym zegarze. Dwoje dzieci odrabia lekcje na tablicy. Ktoś rysuje na tablecie przy playliście. Potem ekran pokazuje start rakiety, grę i muzykę. Kierownicy i pedałów nie ma. Tesla pisze, że taki transport jest bezpieczniejszy, przyjemniejszy i oddaje czas.",
+      },
+      {
+        type: "img",
+        src: "/img/cpub-fotel.jpg",
+        alt: "Wnętrze Cybercaba: fotel, środkowy ekran, brak kierownicy i pedałów",
+        caption: "Fotel tam, gdzie była kierownica. Ekran pośrodku.",
+      },
+      {
+        type: "p",
+        text: "Łącze ma obsłużyć dwie rzeczy naraz. Pasażer streamuje i gra. Auto zostaje w sieci, gdy LTE nie wystarcza. Starlink napisał wprost: V5 da łączność do operacji pojazdu i rozrywki pasażera nawet w miejscach ze słabą albo zatłoczoną komórką. [Musk](https://x.com/elonmusk/status/2095669235993252297) dopiął to jednym zdaniem. Starlink pozwoli oglądać w Cybercabie wideo 4K na żywo. Ten wpis poszedł na 3,3 mln wyświetleń.",
+      },
+      {
+        type: "img",
+        src: "/img/cpub-drzwi.jpg",
+        alt: "Otwarte drzwi złotego Cybercaba z napisem Cybercab",
+        caption: "Dwa miejsca. Wejście tam, gdzie zwykle siedział kierowca.",
+      },
+      {
+        type: "p",
+        text: "To nie jest pierwszy sygnał. 20 lipca Tesla pokazała [przekrój nadwozia](https://x.com/Tesla/status/2079325767260139962) i napisała, że Starlink V5 jest zintegrowany bezpośrednio w Cybercabie. Na grafice antena siedzi z tyłu dachu, obok GPS i LTE. Tamten wpis też zebrał ponad 6 mln wyświetleń. Różnica jest w tonie. W lipcu Tesla pokazała, gdzie antena ma siedzieć. Teraz pokazuje, po co pasażerowi 22 cale, gdy auto jedzie samo, i dostaje pieczątkę z konta Starlinka.",
+      },
+      {
+        type: "img",
+        src: "https://pbs.twimg.com/media/HNs_wwqbEAAuK8Q.jpg",
+        alt: "Przekrój Cybercaba Tesli z anteną Starlink V5 w dachu",
+        caption: "Antena z tyłu dachu, obok GPS i LTE. Źródło: @Tesla, 20 lipca 2026.",
+        contain: true,
+      },
+      {
+        type: "p",
+        text: "Słowo „soon” zostaje. Sprzęt ma wejść, ale Tesla nie podała daty, od której każdy Cybercab wyjedzie z V5 z Giga Texas. Nie podała też, czy V5 zastąpi komórkę, czy tylko ją ubezpiecza. Z oficjalnych zdań wynika to drugie: operacje pojazdu i rozrywka pasażera, gdy LTE słabnie albo się zapycha.",
+      },
+      {
+        type: "img",
+        src: "/img/inv-kabina.jpg",
+        alt: "Kabina Cybercaba: dwa fotele, środkowy ekran, bez kierownicy i pedałów",
+        caption: "Kabina Cybercaba. Dwa miejsca, środkowy ekran, zero kierownicy.",
+      },
+      {
+        type: "p",
+        text: "W wątku wraca to samo pytanie. Właściciele Model Y i Cybertrucków piszą, że chcą tego samego w swoich autach. Tesla na razie pokazuje V5 przy robotaxi. Cybercab bez kierownicy żyje ekranem. Ekran żyje łączem. Komórka w centrum miasta i na pustym odcinku trasy zachowuje się różnie. Satelita ma zasypać tę lukę.",
+      },
+    ],
+  },
+];
