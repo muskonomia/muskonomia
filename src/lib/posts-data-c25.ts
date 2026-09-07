2bb895f63bc4d96ca46df7917400a5839b977455
muskonomia <sebastian.lipinski@gmail.com>
2026-09-06 20:19:12 -0700

    Skrót listy Cybercaba w 25 stanach

--- src/lib/posts-data-c25.ts
+++ src/lib/posts-data-c25.ts
@@ -7,29 +7,37 @@ export const postsC25: Post[] = [
     topic: "tesla",
     title: "Cybercaba widziano już w 25 stanach",
     excerpt:
-      "6 września mapa zgłoszeń Cybercaba liczy 114 lokalizacji w 25 stanach i Dystrykcie Kolumbii. Publiczny kurs z aplikacji nadal jest tylko w Austin.",
+      "6 września mapa zgłoszeń Cybercaba objęła 25 stanów i Dystrykt Kolumbii. Publiczny kurs z aplikacji nadal jest tylko w Austin.",
     date: "6 września 2026",
     isoDate: "2026-09-06",
     img: "/img/cpub-drzwi.jpg",
     body: [
       {
         type: "p",
-        text: "6 września mapa zgłoszeń urosła do 114 lokalizacji i Dystryktu Kolumbii. Tydzień wcześniej lista była krótsza o całe stany. Publiczny kurs z aplikacji nadal jest tylko w Austin.",
+        text: "6 września [Robotaxi Radar](https://x.com/RobotaxiRadar/status/2096667247104438307) złożył mapę zgłoszeń Cybercaba z 25 stanów i Dystryktu Kolumbii. Publiczny kurs z aplikacji nadal jest tylko w Austin. Lista z relacji, bez niezależnej weryfikacji.",
+      },
+      {
+        type: "ul",
+        items: [
+          "15 punktów to sam opis, bez zdjęcia",
+          "Cybercab w aplikacji [Robotaxi](https://x.com/RobotaxiRadar/status/2096667247104438307) od 4 września, tylko wycinek Austin",
+          "reszta mapy: test, transport, parking przy salonie albo przejazd",
+          "[Sawyer Merritt](https://x.com/SawyerMerritt/status/2096282460635943220): ponad 35 sztuk przy lotnisku w Miami",
+          "[Bartlett](https://x.com/RobotaxiRadar/status/2095953016549494928) pod Memphis: 12–15 sztuk przy centrum Tesli; drugi skład w Tennessee jest przy Dover",
+          "po publikacji listy dopisano Queen Creek w Arizonie i Federal Way w stanie Waszyngton",
+          "Bridgeville w Pensylwanii: flota stoi od dwóch tygodni",
+          "4 września [Cybercab Spotter](https://x.com/CybercabSpotter/status/2096186899719156129) naliczył w Austin 233 zgłoszenia w jeden dzień",
+          "niedziela po południu w Austin: Cybercab 40–50 minut, Model Y Robotaxi 18 minut",
+          "rejestr Teksasu na początku września: około 45 Cybercabów; ile bierze kursy, Tesla nie podała",
+          "Model Y bez nadzoru: Austin, Dallas, Houston, Miami, Orlando, Tampa",
+        ],
       },
       {
         type: "img",
         src: "https://pbs.twimg.com/media/HRgJbR8asAAmn7_.jpg",
-        alt: "Mapa USA z 114 lokalizacjami zgłoszeń Cybercaba w 25 stanach i Dystrykcie Kolumbii",
-        caption: "114 lokalizacji, 25 stanów i Dystrykt Kolumbii. 15 punktów bez zdjęcia. [Źródło: @RobotaxiRadar](https://x.com/RobotaxiRadar/status/2096667247104438307).",
+        alt: "Mapa USA ze zgłoszeniami Cybercaba w 25 stanach i Dystrykcie Kolumbii",
+        caption: "25 stanów i Dystrykt Kolumbii. 15 punktów bez zdjęcia. [Źródło: @RobotaxiRadar](https://x.com/RobotaxiRadar/status/2096667247104438307).",
         href: "https://x.com/RobotaxiRadar/status/2096667247104438307",
-      },
-      {
-        type: "p",
-        text: "[Robotaxi Radar](https://x.com/RobotaxiRadar/status/2096667247104438307) złożył zestawienie z publicznych relacji. 15 punktów to sam opis, bez zdjęcia. Nikt tego niezależnie nie weryfikował.",
-      },
-      {
-        type: "p",
-        text: "30 sierpnia ta sama lista wyglądała inaczej. Teksas miał pięć miast. Kalifornia cztery. Floryda cztery. Waszyngton pięć. Reszta kraju mieściła się w jednym worku: Las Vegas, Nashville, Charlotte, Nowy Jork, Chicago, Pittsburgh i stolica. Teraz osobne działy mają Pensylwania, Maryland, Michigan, Arizona, Minnesota i Tennessee.",
       },
       {
         type: "img",
@@ -37,26 +45,6 @@ export const postsC25: Post[] = [
         alt: "Złoty Cybercab z otwartymi drzwiami i napisem Cybercab",
         caption: "Dwa miejsca. Wejście tam, gdzie zwykle siedział kierowca.",
         href: "https://x.com/robotaxi/status/2095707198873571734",
-      },
-      {
-        type: "p",
-        text: "Zgłoszenie to nie kurs. Tesla wpuściła Cybercaba do aplikacji Robotaxi 4 września, w ograniczonym obszarze Austin. Reszta mapy to testy, transport, parking przy salonie albo przejazd. W Charlotte auta kręcą się przy Tesli Northlake od tygodni. W Phoenix ktoś sfotografował złote dwumiejscowe auto i zapytał, kiedy będzie można wezwać kurs. W Orange County też już jeździło.",
-      },
-      {
-        type: "p",
-        text: "Austin po starcie wypełnił się najmocniej. 4 września [Cybercab Spotter](https://x.com/CybercabSpotter/status/2096186899719156129) naliczył 233 zgłoszenia w jeden dzień. To rekord. W rejestrze Teksasu na początku września siedziało około 45 Cybercabów. Ile z nich bierze kursy, Tesla nie podała.",
-      },
-      {
-        type: "p",
-        text: "W wątku od razu poleciały korekty. W Arizonie dopisano Queen Creek. Z Bridgeville w Pensylwanii przyszła informacja, że flota stoi od dwóch tygodni i nikt z okolicy nie widział jej na drodze.",
-      },
-      {
-        type: "p",
-        text: "Kalifornia i Teksas nadal są najgęstsze. W Bay Area punkty siedzą od Fremontu po San Francisco. Na Florydzie lista zeszła z Miami i Orlando do Fort Myers. Osobno wiszą Chicago, Atlanta, Las Vegas i Manhattan.",
-      },
-      {
-        type: "p",
-        text: "Tesla nie ogłosiła kolejnego miasta z Cybercabem w aplikacji. Model Y bez nadzoru jeździ już w Austin, Dallas, Houston, Miami, Orlando i Tampa. Złoty dwumiejscowy dokłada się do tej siatki na razie tylko w Austin.",
       },
     ],
   },
