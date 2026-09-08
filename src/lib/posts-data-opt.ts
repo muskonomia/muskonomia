6363e192cd057b7b9d557cde9165a98e1cc05660
muskonomia <sebastian.lipinski@gmail.com>
2026-09-08 04:48:01 -0700

    Wpis: Tesla zamówiła u dostawców części na 5000 Optimusów

---
+++ src/lib/posts-data-opt.ts
@@ -0,0 +1,55 @@
+import type { Post } from "./posts";
+
+export const postsOpt: Post[] = [
+  {
+    slug: "tesla-zamowila-5000-optimusow",
+    kicker: "Optimus",
+    topic: "tesla",
+    title: "Tesla zamówiła u dostawców części na 5000 Optimusów",
+    excerpt:
+      "Jiemian News podał 7 września, że Tesla złożyła u dostawców zamówienie na części do około 5000 Optimusów. Tesla tego nie potwierdziła. Wewnętrzny plan na 2026 r. to 10–20 tys. sztuk.",
+    date: "8 września 2026",
+    isoDate: "2026-09-08",
+    img: "/img/jpm-optimus.jpg",
+    body: [
+      {
+        type: "p",
+        text: "Chiński [Jiemian News](https://m.jiemian.com/article/15061765.html) podał 7 września, że Tesla złożyła u dostawców z łańcucha T zamówienie na części do około 5000 robotów Optimus. To pierwsza partia liczona w tysiącach po wcześniejszych seriach po kilkaset sztuk. Tesla tego oficjalnie nie potwierdziła.",
+      },
+      {
+        type: "img",
+        src: "/img/jpm-optimus.jpg",
+        alt: "Optimus Tesli",
+        caption: "Optimus. Zamówienie na około 5000 kompletów Tesla oficjalnie nie potwierdziła.",
+      },
+      {
+        type: "p",
+        text: "Jiemian powołuje się na sprawdzanie kanałów u dostawców. Zamówienie poszło na początku września. Wielkości różnią się udziałem danego zakładu. W kwietniu Tesla dała dostawcom wytyczne o produkcji seryjnej. Od tamtej pory nikt nie widział partii w tej skali.",
+      },
+      {
+        type: "p",
+        text: "Linia korpusu we Fremont ruszyła z regulacją maszyn w lipcu. W połowie sierpnia wstępny rozruch był skończony. Wydajność szła w górę. Dopiero wtedy, według tej relacji, Tesla puściła komercyjne części. Bez tego skoku zamówienie na 5000 kompletów nie wychodzi z działu zakupów.",
+      },
+      {
+        type: "p",
+        text: "Wewnętrzny plan Tesli na 2026 r. Jiemian oddaje jako 10–20 tys. sztuk. Część relacji z łańcucha, którą zebrał [Ming](https://x.com/tslaming/status/2096977354044432895), kotwiczy tegoroczną bazę bliżej 15 tys. [Humanoids Daily](https://www.humanoidsdaily.com/news/supply-chain-rumors-hint-at-massive-optimus-order-as-fremont-production-line-ramps-up) powtarza ten sam przedział i dodaje, że te maszyny mają iść najpierw do fabryk Tesli, nie do klientów. Te liczby nie pochodzą z komunikatu Tesli. To to, co dostawcy mówią o papierze, który do nich przyszedł.",
+      },
+      {
+        type: "p",
+        text: "Wytyczne zakupowe, które krążą od lata, są konkretne. We wrześniu dostawcy mają dojść do 1000 kompletów tygodniowo. Na koniec roku tempo ma wynosić 2000–2500 tygodniowo. Jiemian dopisuje, że tygodniówka może jeszcze pójść wyżej niż wcześniejsze 2500. To nadal plan zakupowy, nie raport z taśmy.",
+      },
+      {
+        type: "p",
+        text: "We wrześniu Tesla ma audytować krajowych dostawców. Taki przegląd w jej praktyce przesuwa zakład z puli tymczasowej do certyfikowanego partnera seryjnego. Dostawcy, według tych relacji, już dokładają zapas.",
+      },
+      {
+        type: "p",
+        text: "Chiński rynek zareagował rano 7 września. ETF robotyczny 560770 rósł o 2,15 proc. W czołówce sesji były m.in. Green Harmonic i Orbbec. Sama zwiżka nie mówi, kto dostał ten konkretny komplet. Mówi, że Szanghaj czyta 5000 sztuk jako sygnał z taśmy, nie z sceny.",
+      },
+      {
+        type: "p",
+        text: "Pięć tysięcy kompletów to w skali fabryki aut mało. W skali humanoida to pierwsza liczba, która wygląda jak produkcja, a nie jak seria pokazowa. Tesla milczy. Harmonogramy Optimusa już wcześniej się przesuwały. Różnica teraz jest w papierze. Zamiast slajdu jest zlecenie na części.",
+      },
+    ],
+  },
+];
--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -1,4 +1,5 @@
 import type { Post } from "./posts";
+import { postsOpt } from "./posts-data-opt";
 import { postsFss } from "./posts-data-fss";
 import { postsTxd } from "./posts-data-txd";
 import { postsC25 } from "./posts-data-c25";
@@ -52,6 +53,7 @@ import { postsA } from "./posts-data-a";
 import { postsB } from "./posts-data-b";
 
 export const postsData: Post[] = [
+  ...postsOpt,
   ...postsFss,
   ...postsTxd,
   ...postsC25,
