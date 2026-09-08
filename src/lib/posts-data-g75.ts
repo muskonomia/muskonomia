ac22837da47197dcd59952148d42fd0a9cee3fc8
muskonomia <sebastian.lipinski@gmail.com>
2026-09-08 05:45:36 -0700

    Giga Berlin: akapit o 200 tys. w 2025

--- src/lib/posts-data-g75.ts
+++ src/lib/posts-data-g75.ts
@@ -36,7 +36,7 @@ export const postsG75: Post[] = [
       },
       {
         type: "p",
-        text: "Grünheide robi tylko Model Y na Europę. W 2025 r. zeszło stamtąd nieco ponad 202 tys. sztuk. [Handelsblatt](https://www.handelsblatt.com/unternehmen/industrie/elektromobilitaet-1000-tesla-pro-woche-zu-wenig-sonderschichten-in-gruenheide/100252042.html) oddaje to jako 54 proc. mocy. Zakład stał częściowo pusty. Teraz popyt wraca falami. W sierpniu Tesla zarejestrowała w Niemczech 3034 samochody, o 110,5 proc. więcej niż rok wcześniej. Od stycznia do sierpnia plus wynosi 182 proc., 32 258 sztuk — tak liczy [KBA](https://www.kba.de/DE/Presse/Pressemitteilungen/Fahrzeugzulassungen/2026/pm35_2026_n_08_26_pm_komplett.html).",
+        text: "Zakład w Grünheide produkuje tylko Model Y na Europę. W 2025 r. zeszło stamtąd nieco ponad 200 tys. sztuk. W tym roku ta ilość produkcji ma zostać znacznie pobita. W sierpniu Tesla zarejestrowała w Niemczech 3034 samochody, o 110,5 proc. więcej niż rok wcześniej. Od stycznia do sierpnia plus wynosi 182 proc., 32 258 sztuk — tak liczy [KBA](https://www.kba.de/DE/Presse/Pressemitteilungen/Fahrzeugzulassungen/2026/pm35_2026_n_08_26_pm_komplett.html).",
       },
       {
         type: "p",
