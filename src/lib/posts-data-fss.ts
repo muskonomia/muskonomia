49cd4174eb400f79a7993a8dec61807fb1314d43
muskonomia <sebastian.lipinski@gmail.com>
2026-09-07 18:59:06 -0700

    FSD Słowenia: tylko lead i trzy akapity

--- src/lib/posts-data-fss.ts
+++ src/lib/posts-data-fss.ts
@@ -25,7 +25,7 @@ export const postsFss: Post[] = [
       },
       {
         type: "p",
-        text: "Komunikat zmieścił się w dwóch zdaniach. Do zdjęcia poszła kabina na drodze dwujezdniowej: ręce na udach, na ekranie wizualizacja pasa. Musk powtórzył zdanie Tesli. Słoweńska [24ur i STA](https://www.24ur.com/novice/znanost-in-tehnologija/tesla-v-sloveniji-uvaja-popolnoma-samostojno-voznjo-pod-nadzorom.html) dopisały, że funkcja wejdzie bezprzewodową aktualizacją.",
+        text: "Na liście są Holandia z 10 kwietnia, Litwa i Estonia z maja, Dania i Belgia z czerwca oraz Słowenia z 7 września. Reszta kontynentu zostaje poza listą.",
       },
       {
         type: "img",
@@ -37,27 +37,11 @@ export const postsFss: Post[] = [
       },
       {
         type: "p",
-        text: "Na liście są Holandia z 10 kwietnia, Litwa i Estonia z maja, Dania i Belgia z czerwca oraz Słowenia z 7 września. Reszta kontynentu zostaje poza listą. Holenderski RDW dał wtedy tymczasową homologację pod UN-R-171 i z wyłączeniem części zachowań na mocy unijnego artykułu 39. Kolejne stolice biorą ten papier. Nie czekają na Brukselę.",
-      },
-      {
-        type: "p",
         text: "[Głosowanie](https://x.com/teslaeurope/status/2094661569582096779) Komitetu Technicznego ds. Pojazdów Silnikowych ma się odbyć 6 października. Do tego dnia Tesla otworzyła dane, które wcześniej poszły do RDW. W pięciu krajach system jeździł u ponad 70 tys. klientów, ponad 1 mln km dziennie. Na 100 mln km unijnych dróg użytkownicy FSD mieli 4,1 raza mniej kolizji niż przy jeździe ręcznej. W 2025 r. Europa zanotowała 19 400 ofiar śmiertelnych na drogach, około 53 dziennie.",
       },
       {
         type: "p",
-        text: "[Klub Tesla Owners Slovenia](https://x.com/TeslaOwnersSLO/status/2097046185290654028) podał, co ma wejść do aut. Hardware 4, oprogramowanie v14, aktywacja w ciągu kilku dni. Starszy Hardware 3 poza tą zgodą. Abonament jak w innych krajach Unii: 99 euro miesięcznie, około 427 zł po średnim kursie NBP z 7 września (4,31 zł za euro). Kto wcześniej wziął Enhanced Autopilot, około 49 euro, 211 zł. Miesięczny test Tesla dawała w innych państwach UE. W Słowenii klub jeszcze na to czeka.",
-      },
-      {
-        type: "p",
         text: "Kierowca dalej odpowiada za każdy manewr. Auto z FSD Supervised nie jest autonomiczne. To nadal auto działające pod nadzorem kierowcy.",
-      },
-      {
-        type: "p",
-        text: "Minister infrastruktury Jernej Vrtovec zapowiadał ten krok w lipcu: „Wkrótce też u nas.” Po ogłoszeniu Tesli napisał: „Rozwojowa Słowenia.” Klub podziękował zespołowi Tesli, ministerstwu i agencji bezpieczeństwa ruchu.",
-      },
-      {
-        type: "p",
-        text: "W Niemczech, Francji i we Włoszech funkcji nadal nie ma. To samo auto, inny pakiet. Słowenia tę granicę właśnie przesunęła.",
       },
     ],
   },
