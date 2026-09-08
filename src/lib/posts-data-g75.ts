d5f049105390ed0acd91d9e506a5b9746aae4682
muskonomia <sebastian.lipinski@gmail.com>
2026-09-08 05:41:10 -0700

    Giga Berlin: klikalne źródło Muskonomy

--- src/lib/posts-data-g75.ts
+++ src/lib/posts-data-g75.ts
@@ -40,7 +40,7 @@ export const postsG75: Post[] = [
       },
       {
         type: "p",
-        text: "Na X ta wiadomość idzie jako dowód, że problemem nie jest brak zamówień. [Kanał Giga Berlin](https://x.com/Gf4Tesla/status/2096857595612983330) powtarza te same liczby i kładzie ciężar na popyt. Obok wraca Volkswagen: kolejne 50 tys. etatów do cięcia i pytanie o Emden, Zwickau, Hanower i Neckarsulm. Tam mocy jest za dużo. Tu Tesli brakuje około 1000 aut na tydzień.",
+        text: "Na X ta wiadomość idzie jako dowód, że problemem nie jest brak zamówień. [Muskonomy](https://x.com/muskonomy/status/2097008733633290729) zebrał liczby z Handelsblattu. [Kanał Giga Berlin](https://x.com/Gf4Tesla/status/2096857595612983330) powtarza to samo i kładzie ciężar na popyt. Obok wraca Volkswagen: kolejne 50 tys. etatów do cięcia i pytanie o Emden, Zwickau, Hanower i Neckarsulm. Tam mocy jest za dużo. Tu Tesli brakuje około 1000 aut na tydzień.",
       },
     ],
   },
