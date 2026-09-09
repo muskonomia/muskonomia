53991e220e2710eb831a9126504df63f82717a3d
muskonomia <sebastian.lipinski@gmail.com>
2026-09-09 03:44:06 -0700

    Unboxed: klatka w tekscie otwiera film na X

--- src/lib/posts-data-ubx.ts
+++ src/lib/posts-data-ubx.ts
@@ -17,10 +17,11 @@ export const postsUbx: Post[] = [
         text: "8 września [konto Robotaxi](https://x.com/robotaxi/status/2097422261624193106) puściło film z linii Cybercaba w Giga Texas. Moduły powstają równolegle, a całość jest złożona w jednym kroku na końcu. Linia ma zajmować o połowę mniej miejsca. Musk napisał, że produkcję przeprojektowano tak, by szła ponad 5 razy szybciej niż klasyczna.",
       },
       {
-        type: "video",
-        src: "/video/cybercab-unboxed-linia.mp4",
-        poster: "/img/cybercab-unboxed-linia.jpg",
-        caption: "Linia Unboxed Cybercaba w Giga Texas. [Źródło: @robotaxi](https://x.com/robotaxi/status/2097422261624193106).",
+        type: "img",
+        src: "/img/cybercab-unboxed-linia.jpg",
+        alt: "Ujęcie z filmu linii Unboxed Cybercaba w Giga Texas",
+        caption: "Ujęcie z filmu. Kliknij, żeby otworzyć nagranie na X. [Źródło: @robotaxi](https://x.com/robotaxi/status/2097422261624193106).",
+        href: "https://x.com/robotaxi/status/2097422261624193106",
       },
       {
         type: "p",
