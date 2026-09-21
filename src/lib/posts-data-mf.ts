d21a6c1d38e2a2018a5c50bde82d94bdafa1c805
muskonomia <sebastian.lipinski@gmail.com>
2026-09-21 06:28:51 +0000

    Add Musk firsts column from AsFoundX collage

---
+++ src/lib/posts-data-mf.ts
@@ -0,0 +1,99 @@
+import type { Post } from "./posts";
+
+export const postsMf: Post[] = [
+  {
+    slug: "musk-a-kolejne-pierwsze-razy",
+    kicker: "Elon",
+    topic: "inne",
+    title: "Musk a kolejne pierwsze razy",
+    excerpt:
+      "Nikt inny nie zebrał tylu pierwszych razy co Musk. Od Falcona 1 i Roadstera po złapanie Super Heavy i Cybercaba bez kierownicy. Dalej Księżyc i Mars.",
+    date: "21 września 2026",
+    isoDate: "2026-09-21",
+    img: "/img/mf-kolaz.jpg",
+    contain: true,
+    body: [
+      {
+        type: "p",
+        text: "Nikt inny na świecie nie zebrał tylu pierwszych razy co Elon Musk. Poniżej najważniejsze. I na pewno to nie są ostatnie. Jako redakcja jesteśmy ciekawi, co będzie kolejnym pierwszym razem w firmach Elona Muska.",
+      },
+      {
+        type: "img",
+        src: "/img/mf-kolaz.jpg",
+        alt: "Kolaż dziesięciu pierwszych razy Muska: Falcon 1, Dragon, lądowanie Falcona 9, Crew Dragon, Inspiration4, Polaris Dawn, złapanie Super Heavy, Roadster",
+        caption: "Od 2008 do 2024. Źródło: @AsFoundX.",
+        contain: true,
+        href: "https://x.com/AsFoundX/status/2101673824613728564",
+      },
+      { type: "x", id: "2101673824613728564", handle: "AsFoundX" },
+      {
+        type: "p",
+        text: "Pierwsza prywatna rakieta na paliwo ciekłe, która weszła na orbitę. Falcon 1, czwarty lot, wrzesień 2008. Trzy wcześniejsze poszły w ocean. Czwarty został.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy seryjny elektryk z zasięgiem ponad 200 mil. Roadster, też 2008. Dwieście mil to wtedy była granica, za którą reszta branży jeszcze nie stała.",
+      },
+      {
+        type: "p",
+        text: "Pierwsza prywatna firma, która wyniosła statek i ściągnęła go z powrotem. Dragon, grudzień 2010.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy komercyjny statek przy ISS. Dragon, maj 2012. NASA przestała mieć monopol na dostawy na stację.",
+      },
+      {
+        type: "p",
+        text: "Pierwsze pionowe, napędowe lądowanie rakiety orbitalnej. Falcon 9, grudzień 2015, LZ-1. Blue Origin lądował wcześniej, ale suborbitalnie. Tu wrócił booster, który był na orbicie.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy ponowny lot i lądowanie zużytego boostera orbitalnego. Marzec 2017, SES-10. Od tamtej pory „używana rakieta” przestała być żartem.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy komercyjny załogowy lot orbitalny z terenu USA. Demo-2, maj 2020. Po wahadłowcach Ameryka wróciła na orbitę własnym statkiem, tylko że tym razem prywatnym.",
+      },
+      {
+        type: "p",
+        text: "Pierwsza w pełni prywatna misja orbitalna. Inspiration4, wrzesień 2021. Żadnego astronauty z ramienia agencji. Cztery osoby, Dragon, trzy dni.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy komercyjny spacer kosmiczny. Polaris Dawn, wrzesień 2024. Isaacman i Gillis wychodzą z hatcha. Nie z wahadłowca, nie z Sojuza.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy złapany booster. Super Heavy, październik 2024. Wieże Mechazilla biorą 70-metrowy pierwszy stopień w ramiona. Tego wcześniej nie było nawet na rysunku, który ktoś traktował serio.",
+      },
+      {
+        type: "img",
+        src: "/img/starship-tower-night.jpg",
+        alt: "Wieże Mechazilla przy Starshipie nocą w Starbase",
+        caption: "Wieże, które biorą booster. Starbase.",
+      },
+      {
+        type: "p",
+        text: "Pierwszy seryjny samochód bez kierownicy i pedałów. Cybercab. Zaprojektowany od zera pod jazdę bez kierowcy, dwa miejsca, ławka, ekran pośrodku. Od 4 września w Austin zamawia się go z aplikacji. To już nie jest teaser. To auto, które jedzie po zwykłej ulicy, a tam, gdzie zwykle siedział kierowca, odchyla się fotel.",
+      },
+      {
+        type: "img",
+        src: "/img/cover-cybercab.jpg",
+        alt: "Złoty Tesla Cybercab bez kierownicy",
+        caption: "Cybercab. Bez kierownicy, bez pedałów. Austin, z aplikacji.",
+      },
+      {
+        type: "p",
+        text: "Dalej lista dopiero się otwiera. SpaceX powtarza to samo od lat: miasto na Księżycu szybciej, bo start co dziesięć dni i dwa dni lotu. Mars wolniej: okno co 26 miesięcy, sześć miesięcy w drodze. Baza na Księżycu ma być możliwa poniżej dekady. Miasto na Marsie — za 5–7 lat, w dłuższym horyzoncie 20 plus. Optimus na linii, kolejny Cybercab poza Austin. Które z tego wejdzie jako następne „pierwsze”.",
+      },
+      {
+        type: "p",
+        text: "Kolaż z wpisu kończy się na 2024. Cybercab już wyjechał. Księżyc i Mars jeszcze nie. Jako redakcja czekamy na jedenaste.",
+      },
+      {
+        type: "p",
+        text: "Źródło: [As Found](https://x.com/AsFoundX/status/2101673824613728564).",
+      },
+    ],
+  },
+];
--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -1,4 +1,5 @@
 import type { Post } from "./posts";
+import { postsMf } from "./posts-data-mf";
 import { postsGt } from "./posts-data-gt";
 import { postsRz } from "./posts-data-rz";
 import { postsG3 } from "./posts-data-g3";
@@ -83,6 +84,7 @@ import { postsA } from "./posts-data-a";
 import { postsB } from "./posts-data-b";
 
 export const postsData: Post[] = [
+  ...postsMf,
   ...postsGt,
   ...postsRz,
   ...postsG3,
