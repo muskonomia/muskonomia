2396b09b3f03c2994afafb7f202c3a6030f03611
muskonomia <sebastian.lipinski@gmail.com>
2026-09-21 06:34:19 +0000

    Split Musk firsts collage into separate photos, drop AsFoundX embed

--- src/lib/posts-data-mf.ts
+++ src/lib/posts-data-mf.ts
@@ -10,57 +10,101 @@ export const postsMf: Post[] = [
       "Nikt inny nie zebrał tylu pierwszych razy co Musk. Od Falcona 1 i Roadstera po złapanie Super Heavy i Cybercaba bez kierownicy. Dalej Księżyc i Mars.",
     date: "21 września 2026",
     isoDate: "2026-09-21",
-    img: "/img/mf-kolaz.jpg",
-    contain: true,
+    img: "/img/mf-superheavy.jpg",
     body: [
       {
         type: "p",
         text: "Nikt inny na świecie nie zebrał tylu pierwszych razy co Elon Musk. Poniżej najważniejsze. I na pewno to nie są ostatnie. Jako redakcja jesteśmy ciekawi, co będzie kolejnym pierwszym razem w firmach Elona Muska.",
       },
       {
-        type: "img",
-        src: "/img/mf-kolaz.jpg",
-        alt: "Kolaż dziesięciu pierwszych razy Muska: Falcon 1, Dragon, lądowanie Falcona 9, Crew Dragon, Inspiration4, Polaris Dawn, złapanie Super Heavy, Roadster",
-        caption: "Od 2008 do 2024. Źródło: @AsFoundX.",
-        contain: true,
-        href: "https://x.com/AsFoundX/status/2101673824613728564",
-      },
-      { type: "x", id: "2101673824613728564", handle: "AsFoundX" },
-      {
         type: "p",
         text: "Pierwsza prywatna rakieta na paliwo ciekłe, która weszła na orbitę. Falcon 1, czwarty lot, wrzesień 2008. Trzy wcześniejsze poszły w ocean. Czwarty został.",
+      },
+      {
+        type: "img",
+        src: "/img/mf-falcon1.jpg",
+        alt: "Start Falcona 1, 2008",
+        caption: "Falcon 1. 2008.",
       },
       {
         type: "p",
         text: "Pierwszy seryjny elektryk z zasięgiem ponad 200 mil. Roadster, też 2008. Dwieście mil to wtedy była granica, za którą reszta branży jeszcze nie stała.",
       },
       {
+        type: "img",
+        src: "/img/mf-roadster.jpg",
+        alt: "Czerwony Tesla Roadster z 2008",
+        caption: "Roadster. 2008.",
+      },
+      {
         type: "p",
         text: "Pierwsza prywatna firma, która wyniosła statek i ściągnęła go z powrotem. Dragon, grudzień 2010.",
+      },
+      {
+        type: "img",
+        src: "/img/mf-dragon2010.jpg",
+        alt: "Kapsuła Dragon wyławiana z oceanu, 2010",
+        caption: "Dragon. 2010.",
       },
       {
         type: "p",
         text: "Pierwszy komercyjny statek przy ISS. Dragon, maj 2012. NASA przestała mieć monopol na dostawy na stację.",
       },
       {
+        type: "img",
+        src: "/img/mf-dragon2012.jpg",
+        alt: "Dragon zadokowany przy Międzynarodowej Stacji Kosmicznej, 2012",
+        caption: "Dragon przy ISS. 2012.",
+      },
+      {
         type: "p",
         text: "Pierwsze pionowe, napędowe lądowanie rakiety orbitalnej. Falcon 9, grudzień 2015, LZ-1. Blue Origin lądował wcześniej, ale suborbitalnie. Tu wrócił booster, który był na orbicie.",
+      },
+      {
+        type: "img",
+        src: "/img/mf-falcon9-2015.jpg",
+        alt: "Falcon 9 ląduje pionowo w nocy, 2015",
+        caption: "Falcon 9, LZ-1. 2015.",
       },
       {
         type: "p",
         text: "Pierwszy ponowny lot i lądowanie zużytego boostera orbitalnego. Marzec 2017, SES-10. Od tamtej pory „używana rakieta” przestała być żartem.",
       },
       {
+        type: "img",
+        src: "/img/mf-falcon9-2017.jpg",
+        alt: "Ponowny lot Falcona 9, 2017",
+        caption: "Falcon 9, ponowny lot. 2017.",
+      },
+      {
         type: "p",
         text: "Pierwszy komercyjny załogowy lot orbitalny z terenu USA. Demo-2, maj 2020. Po wahadłowcach Ameryka wróciła na orbitę własnym statkiem, tylko że tym razem prywatnym.",
+      },
+      {
+        type: "img",
+        src: "/img/mf-crewdragon.jpg",
+        alt: "Start Crew Dragona z Kennedy Space Center, 2020",
+        caption: "Crew Dragon, Demo-2. 2020.",
       },
       {
         type: "p",
         text: "Pierwsza w pełni prywatna misja orbitalna. Inspiration4, wrzesień 2021. Żadnego astronauty z ramienia agencji. Cztery osoby, Dragon, trzy dni.",
       },
       {
+        type: "img",
+        src: "/img/mf-inspiration4.jpg",
+        alt: "Łuk startu Inspiration4 nad Florydą, 2021",
+        caption: "Inspiration4. 2021.",
+      },
+      {
         type: "p",
         text: "Pierwszy komercyjny spacer kosmiczny. Polaris Dawn, wrzesień 2024. Isaacman i Gillis wychodzą z hatcha. Nie z wahadłowca, nie z Sojuza.",
+      },
+      {
+        type: "img",
+        src: "/img/mf-polaris.jpg",
+        alt: "Spacer kosmiczny Polaris Dawn, 2024",
+        caption: "Polaris Dawn. 2024.",
       },
       {
         type: "p",
@@ -68,9 +112,9 @@ export const postsMf: Post[] = [
       },
       {
         type: "img",
-        src: "/img/starship-tower-night.jpg",
-        alt: "Wieże Mechazilla przy Starshipie nocą w Starbase",
-        caption: "Wieże, które biorą booster. Starbase.",
+        src: "/img/mf-superheavy.jpg",
+        alt: "Wieże Mechazilla łapią booster Super Heavy, 2024",
+        caption: "Super Heavy. 2024.",
       },
       {
         type: "p",
@@ -88,11 +132,7 @@ export const postsMf: Post[] = [
       },
       {
         type: "p",
-        text: "Kolaż z wpisu kończy się na 2024. Cybercab już wyjechał. Księżyc i Mars jeszcze nie. Jako redakcja czekamy na jedenaste.",
-      },
-      {
-        type: "p",
-        text: "Źródło: [As Found](https://x.com/AsFoundX/status/2101673824613728564).",
+        text: "Lista z 2024 kończy się na złapaniu boostera. Cybercab już wyjechał. Księżyc i Mars jeszcze nie. Jako redakcja czekamy na jedenaste.",
       },
     ],
   },
