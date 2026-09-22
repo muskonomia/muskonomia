5790fa8f7f9f9dbc48081187ba0324e11143e718
muskonomia <sebastian.lipinski@gmail.com>
2026-09-22 19:18:24 +0000

    Publish Starship Holy Grail ocean recovery film teaser

---
+++ src/lib/posts-data-graal.ts
@@ -0,0 +1,37 @@
+import type { Post } from "./posts";
+
+export const postsGraal: Post[] = [
+  {
+    slug: "film-starship-swiety-graal",
+    kicker: "Starship",
+    topic: "spacex",
+    title: "SpaceX wrzucił film o odzysku Starshipa z oceanu",
+    excerpt:
+      "SpaceX wrzucił trzeci odcinek serii o Starshipie. Dokument o odzysku z oceanu — warto zobaczyć ten i dwa wcześniejsze.",
+    date: "22 września 2026",
+    isoDate: "2026-09-22",
+    img: "/img/graal-okladka.jpg",
+    body: [
+      {
+        type: "p",
+        text: "SpaceX wrzucił trzeci odcinek serii o Starshipie. [„Starship — The Holy Grail of Rocketry”](https://www.youtube.com/watch?v=EthkNLasUa8) to dokument o wielokrotnym locie — od Grasshoppera i Falcona do Ship 40 na Oceanie Indyjskim.",
+      },
+      {
+        type: "img",
+        src: "/img/graal-okladka.jpg",
+        alt: "Kadr z filmu SpaceX Starship — The Holy Grail of Rocketry",
+        caption:
+          "[Obejrzyj na YouTube](https://www.youtube.com/watch?v=EthkNLasUa8). 28 minut. Trzeci odcinek serii STARSHIP.",
+        href: "https://www.youtube.com/watch?v=EthkNLasUa8",
+      },
+      {
+        type: "p",
+        text: "Kto lubi rakiety i piękne kadry, naprawdę warto zobaczyć. Formalnie to dokument, ale ekipa wskoczyła poziom wyżej w tym, jak da się taką historię opowiedzieć i pokazać. Fan SpaceX, rakiet i kosmosu powinien obejrzeć ten odcinek i dwa wcześniejsze.",
+      },
+      {
+        type: "p",
+        text: "[Obejrzyj na YouTube](https://www.youtube.com/watch?v=EthkNLasUa8).",
+      },
+    ],
+  },
+];
--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -1,4 +1,5 @@
 import type { Post } from "./posts";
+import { postsGraal } from "./posts-data-graal";
 import { postsLsw } from "./posts-data-lsw";
 import { postsKe } from "./posts-data-ke";
 import { postsDsx } from "./posts-data-dsx";
@@ -91,6 +92,7 @@ import { postsA } from "./posts-data-a";
 import { postsB } from "./posts-data-b";
 
 export const postsData: Post[] = [
+  ...postsGraal,
   ...postsLsw,
   ...postsKe,
   ...postsDsx,
