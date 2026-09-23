55ddc13228acb6e4eb0455ee3bef6d5311ef0416
muskonomia <sebastian.lipinski@gmail.com>
2026-09-23 12:38:10 -0700

    Dodaj piksel X (rfr6q) w head na wszystkich stronach.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,11 @@ import appCss from "../styles.css?url";
 
 const APP_NAME = "muskonomia.pl";
 
+const X_PIXEL = `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','rfr6q');`;
+
 export const Route = createRootRoute({
   head: () => ({
     meta: [
@@ -33,6 +38,7 @@ export const Route = createRootRoute({
     <html lang="pl" suppressHydrationWarning>
       <head>
         <HeadContent />
+        <script dangerouslySetInnerHTML={{ __html: X_PIXEL }} />
       </head>
       <body>
         <PreviewHostBridge />
