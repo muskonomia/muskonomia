f645a3f0f066d221d2b384b0d7926e9d24ea3507
muskonomia <sebastian.lipinski@gmail.com>
2026-09-24 11:40:08 +0000

    Skróć artykuł o katodzie Cybercaba do pięciu akapitów.

--- src/lib/posts-data-ckat.ts
+++ src/lib/posts-data-ckat.ts
@@ -14,7 +14,7 @@ export const postsCkat: Post[] = [
     body: [
       {
         type: "p",
-        text: "Tesla ogłosiła pierwszego Cybercaba z materiałem katodowym ze swojej fabryki w Austin. Konto Robotaxi pokazało złote dwuosobowe auto z uniesionymi drzwiami i duży biały worek proszku na palecie. Obok stoi ekipa z Giga Texas. Zakład, z którego wyszedł ten proszek, firma od sierpnia nazywa pierwszą wielkoskalową produkcją katody w Ameryce Północnej.",
+        text: "Tesla ogłosiła pierwszego Cybercaba z materiałem katodowym ze swojej fabryki w Austin. Zakład, z którego wyszedł ten proszek, firma od sierpnia nazywa pierwszą wielkoskalową produkcją katody w Ameryce Północnej.",
       },
       {
         type: "img",
@@ -25,44 +25,25 @@ export const postsCkat: Post[] = [
         href: "https://x.com/robotaxi/status/2102870359511105606",
       },
       { type: "x", id: "2102870359511105606", handle: "robotaxi" },
+      { type: "x", id: "2102871471949271501", handle: "CernBasher" },
       {
         type: "p",
         text: "To nie jest pierwszy Cybercab z taśmy. Produkcja dwuosobowego robotaxi w Austin ruszyła wcześniej w tym roku. Zmieniło się źródło najdroższego składnika ogniwa. Katoda siedzi w ogniwach 4680, które Tesla robi w tym samym kompleksie.",
       },
       {
         type: "p",
-        text: "Musk dopisał, że auto złożyli z niklowej katody wyprodukowanej lokalnie w Gigafactory Texas. Wiceprezes programu 4680, Bonne Eggleston, skwitował zdjęcie krótko: „pierwszy z wielu”.",
+        text: "Musk dopisał, że auto złożyli z niklowej katody wyprodukowanej lokalnie w Gigafactory Texas. Wiceprezes programu 4680, Bonne Eggleston, skwitował to krótko: „pierwszy z wielu”.",
       },
       { type: "x", id: "2102926113173627069", handle: "elonmusk" },
       {
         type: "p",
-        text: "Katoda to dodatnia elektroda ogniwa. Decyduje o pojemności, żywotności i sporze o koszt. Przez lata wielkoskalowa produkcja tego proszku siedziała poza Ameryką Północną, głównie w Azji. Tesla kupowała materiał, skręcała ogniwo w Teksasie i składała auto. Teraz proszek, ogniwo i Cybercab wychodzą z jednego kampusu.",
+        text: "Przez lata wielkoskalowa produkcja tego proszku siedziała poza Ameryką Północną, głównie w Azji. Tesla kupowała materiał, skręcała ogniwo w Teksasie i składała auto. Teraz proszek, ogniwo i Cybercab wychodzą z jednego kampusu.",
       },
       {
         type: "p",
-        text: "21 sierpnia konto Tesla North America puściło pierwsze wideo z wnętrza linii katodowej w Austin. Podpis brzmiał tak samo jak teraz: pierwsza wielkoskalowa produkcja katody w Ameryce Północnej, przy ogniwach 4680. Wtedy Tesla pokazała halę. Teraz pokazuje auto, które ten materiał już ma w pakiecie.",
+        text: "21 sierpnia Tesla North America pokazała wnętrze linii katodowej w Austin. Wtedy firma pokazała halę. Teraz pokazuje auto, które ten materiał już ma w pakiecie.",
       },
       { type: "x", id: "2090901581420253482", handle: "tesla_na" },
-      {
-        type: "p",
-        text: "Austin American-Statesman pisał po tamtym filmie o ofertach pracy przy katodzie w Giga Texas. W ogłoszeniach Tesla opisywała projekt kapitałowy wart ponad 100 mln dolarów, około 386 mln zł po kursie NBP z 24 września 2026 (1 USD = 3,8570 zł). Tonażu linii firma nadal nie podaje.",
-      },
-      {
-        type: "p",
-        text: "To osobny odcinek od litu z rafinerii pod Corpus Christi. Tam Tesla przerabia koncentrat spodumenu na wodorotlenek litowy. Ten lit już wcześniej trafił do seryjnego Cybertrucka. Katoda to kolejny stopień: prekursory niklu, manganu i kobaltu miesza się z wodorotlenkiem, praży i czyści. Z tego powstaje proszek, który idzie na folię aluminiową. Dwa zakłady, dwa etapy, jeden stan.",
-      },
-      {
-        type: "p",
-        text: "Tesla nie podała, ile tego proszku linia w Austin robi na godzinę, jaki udział floty Cybercabów już jedzie na własnej katodzie i o ile taniej wychodzi ogniwo. Nie podała też, czy ten sam materiał wchodzi już seryjnie do Modelu Y i Semi z teksaskich 4680. Zdjęcie mówi tylko o pierwszym Cybercabie z tym wsadem.",
-      },
-      {
-        type: "p",
-        text: "Joe Tegtmeyer z Austin pogratulował ekipie zakładu litowego i zespołowi baterii w Giga Texas. Na hali widać skutek kilku lat roboty, nie slajd z Battery Day. Tam Tesla zapowiadała, że chemię ogniwa chce trzymać u siebie. Worek przy złotym aucie to ta chemia w wersji, którą da się sfotografować.",
-      },
-      {
-        type: "p",
-        text: "Ile takich worków wejdzie w przyszłoroczną flotę robotaxi, z tego kadru nie wynika. Wynika coś skromniejszego. Tesla nie czeka z własną chemią na pełny rozruch usługi. Najpierw złożyła auto na swoim proszku.",
-      },
     ],
   },
 ];
