927f3ebb7150edf4a8d2272e01fb8af88499968e
muskonomia <sebastian.lipinski@gmail.com>
2026-09-25 07:43:34 +0000

    Nowa okładka Colossusa: kampus Minihard.

--- src/lib/posts-data-col2.ts
+++ src/lib/posts-data-col2.ts
@@ -10,7 +10,7 @@ export const postsCol2: Post[] = [
       "W Colossusie pracuje dziś 780 tys. GPU. W przyszłym tygodniu dojdzie 220 tys. GB300 i klaster przekroczy milion.",
     date: "25 września 2026",
     isoDate: "2026-09-25",
-    img: "/img/spacex-ai-colossus.jpg",
+    img: "/img/minihard-gb300.jpg",
     body: [
       {
         type: "p",
